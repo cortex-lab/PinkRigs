2b51ecbdea343a1dd686f5780863e0462fce3c70
timothysit <sitpakhag@gmail.com>
2021-11-15 18:54:43 +0000

    added code to run facemap at specific time of day

---
+++ Zelda-Time/automatic_facemap.py
@@ -0,0 +1,24 @@
+import os
+import facemap
+import datetime
+import time
+import batch_process_pinkavrig_videos as bprocess
+
+
+def main():
+    how_often_to_check = 3600
+    continue_running = True
+
+    while continue_running:
+        e = datetime.datetime.now()
+        print("The time is now: %s:%s:%s" % (e.hour, e.minute, e.second))
+        if (e.hour < 8) & (e.hour > 20):
+            print('It is prime time to run some facemap!')
+            bprocess.main()
+
+        else:
+            print('Hi Magda, it is between 8am - 8pm, I will sleep a bit and check back later')
+            time.sleep(how_often_to_check)
+
+if __name__ == '__main__':
+    main()
