7603b5882eacd17ff5e3daf7e29cc57472d4165d
Célian Bimbard <54884591+cbimbo@users.noreply.github.com>
2021-12-07 15:17:47 +0000

    Update alignVideo_AVrigs.m

--- Analysis/+align/alignVideo_AVrigs.m
+++ Analysis/+align/alignVideo_AVrigs.m
@@ -1,147 +1,181 @@
-function alignVideo_AVrigs(mouseName, thisDate, expNum, movieName, varargin)
+function [tls, blocks, expNums, eTimeline2keep, eBlock2keep, tags, alignDir] = alignEphys_AVrigs(subject, date, P)
     
-    %%
-    % Need to make it much more general, including:
-    % - file names
-    % - video types
-    % - etc.
+    subjectsFolder = getRootDir(subject, date);
+    alignDir = fullfile(subjectsFolder, 'alignments');
+    if ~exist(alignDir,'dir')
+        mkdir(alignDir)
+    end
     
-    %%
-    timelineExpNums = expNum;
-    tlSyncName = 'camSync';
-    recompute = false;
-    nFramesToLoad = 3000;
-    if ~isempty(varargin)
-        params = varargin{1};
-        if isfield(params, 'recompute')
-            recompute = params.recompute;
+    [tags, hasEphys] = getEphysTags(subject, date);
+    
+    % focus on audioVis
+    tags = tags(cellfun(@(x) ~isempty(x), strfind(tags,[P.exp '_g'])));
+    
+    % determine what exp nums exist
+    [expNums, blocks, hasBlock, pars, isMpep, tls, hasTimeline] = ...
+        whichExpNums(subject, date); % used to be dat.whichExpNums?
+    
+    useFlipper = true; % no idea why empty
+    
+    %% align times (timeline to ephys)
+    
+    %%% not that there should be only 1 exp. Not sure if it will work with
+    %%% several of them.
+    
+    % for any ephys, load the sync data
+    if hasEphys
+        for t = 1:length(tags)
+            if isempty(tags{t})
+                [~, pdFlips, allET] = loadSyncChronic(subject, date);
+            else
+                [~, pdFlips, allET] = loadSyncChronic(subject, date, tags{t});
+            end
+            if useFlipper
+                ephysFlips{t} = allET; % problem with big files, had to bypass spikeGLXdigitalParse
+                %             ephysFlips{t} = allET{7}{1};
+            else
+                ephysFlips{t} = pdFlips;
+            end
         end
-        if isfield(params, 'nFramesToLoad')
-            nFramesToLoad = params.nFramesToLoad;
-        end
     end
-    
-    %%
-    % should have a way to generalize (dat.expFilePath should be helpful,
-    % but isn't working with the current split in servers...
-    % Should insist a bit more though)
-    server = '\\znas.cortexlab.net\Subjects\';
-    if ~exist(fullfile(server,mouseName, thisDate, num2str(expNum)),'dir')
-        server = '\\128.40.224.65\Subjects\';
-    end
-    movieDir = fullfile(server, mouseName, thisDate, num2str(expNum));
-    intensFile = fullfile(server, mouseName, thisDate, num2str(expNum), ...
-        [movieName '_avgIntensity.mat']);
-    % have to deal with the "lastFrames" file. Pretty annoying.
-    intensFile_lastFrames = fullfile(server, mouseName, thisDate, num2str(expNum), ...
-        [movieName '_lastFrames_avgIntensity.mat']);
-    
-    if recompute && exist(intensFile, 'file')
-        delete(intensFile);
-    end
-    if recompute && exist(intensFile_lastFrames, 'file')
-        delete(intensFile_lastFrames);
-    end
-    
-    if ~exist(intensFile, 'file')
-        fprintf(1, 'computing average intensity of first/last frames...\n');
-        ROI = avgMovieIntensity(movieDir, movieName, [], true, [], [], nFramesToLoad);
-    end
-    if ~exist(intensFile_lastFrames, 'file')
-        ROI_lastFrames = avgMovieIntensity(movieDir, [movieName '_lastFrames'], [], true, [], [], []);
-    end
-    
-    fprintf(1, 'loading avg intensity\n');
-    load(intensFile);
-    lf = load(intensFile_lastFrames);
-    avgIntensity = [avgIntensity lf.avgIntensity];
-    
-    %% first detect the pulses in the avgIntensity trace
-    
-    expectedNumSyncs = numel(timelineExpNums)*2; % one at the beginning and end of each timeline file
-    
-    vidIntensThresh = [15 20];
-    % [intensTimes, intensUp, intensDown] = schmittTimes(1:numel(avgIntensity), avgIntensity, vidIntensThresh);
-    intensDown = find(diff(avgIntensity)>10); intensDown(find(diff(intensDown)<2)+1) = [];
-    attemptNum = 1; loadAttemptNum = 1;
-    while(numel(intensDown)~=expectedNumSyncs)
-        % try some different approaches to get the right threshold
-        % automatically...
-        switch attemptNum
-            case 1
-                vidIntensThresh = min(avgIntensity)*[1.2 1.4];
-            case 2
-                intensMed = median(avgIntensity);
-                intensMin = min(avgIntensity);
-                vidIntensThresh = intensMin+(intensMed-intensMin)*[0.4 0.6];
-            case 3
-                vidIntensThresh = intensMin+(intensMed-intensMin)*[0.15 0.25];
-            otherwise
-                switch loadAttemptNum
-                    case 1
-                        fprintf(1, 'trying to load more frames...\n')
-                        avgMovieIntensity(movieDir, movieName, [], true, ROI, [], 10000);
-                        load(intensFile); avgIntensity = [avgIntensity lf.avgIntensity];
-                        attemptNum = 0;
-                    case 2
-                        fprintf(1, 'trying to load all frames...\n')
-                        avgMovieIntensity(movieDir, movieName, [], true, ROI);
-                        load(intensFile); avgIntensity = [avgIntensity lf.avgIntensity];
-                        attemptNum = 0;
-                    otherwise
-                        fprintf(1, 'cannot find a threshold that works. You tell me...\n');
-                        figure; plot(avgIntensity);
-                        keyboard
-                end
-                loadAttemptNum = loadAttemptNum+1;
-        end
         
-        [intensTimes, intensUp, intensDown] = schmittTimes(1:numel(avgIntensity), avgIntensity, vidIntensThresh);
-        attemptNum = attemptNum +1;
+    % detect sync events from timelines
+    tlFlips = {};
+    for e = 1:length(expNums)
+        if hasTimeline(e)
+            Timeline = tls{e};
+            tt = Timeline.rawDAQTimestamps;
+            if useFlipper
+                evTrace = Timeline.rawDAQData(:, strcmp({Timeline.hw.inputs.name}, 'flipper'));
+                evT = schmittTimes(tt, evTrace, [3 4]); % all flips, both up and down
+            else
+                evTrace = Timeline.rawDAQData(:, strcmp({Timeline.hw.inputs.name}, 'photoDiode'));
+                evT = schmittTimes(tt, evTrace, [3 4]); % all flips, both up and down
+                evT = evT([true; diff(evT)>0.2]);
+            end
+            tlFlips{e} = evT;
+        end
     end
-    assert(numel(intensDown)==expectedNumSyncs, 'could not find correct number of syncs');
-    fprintf(1, 'found the sync pulses in the video\n');
     
-    %% now get the timings
-    % usually these are TTL and pretty much anything between 0 adn 5 will work
-    % but here I use a small value because for the whisker camera I didn't have
-    % TTL so this works for that too.
-    % tlStrobeThresh = [0.08 0.15];
-    tlStrobeThresh = [1 2];
-    tlSyncThresh = [2 3];
+    % match up ephys and timeline events:
+    % algorithm here is to go through each timeline available, figure out
+    % whether the events in timeline align with any of those in the ephys. If
+    % so, we have a conversion of events in that timeline into ephys
+    %
+    % Only align to the first ephys recording, since the other ones are aligned
+    % to that
+    eTimeline2keep = [];
+    if hasEphys
+        ef = ephysFlips{1};
+        if useFlipper && ef(1)<0.001
+            % this happens when the flipper was in the high state to begin with
+            % - a turning on event is registered at the first sample. But here
+            % we need to drop it.
+            ef = ef(2:end);
+        end
+        for e = 1:length(expNums)
+            if hasTimeline(e)
+                fprintf('trying to correct timeline %d to ephys\n', expNums(e));
+                %Timeline = tl{e};
+                tlT = tlFlips{e};
+                
+                success=false;
+                if length(tlT)==length(ef)
+                    % easy case: the two are exactly coextensive
+                    [~,b] = makeCorrection(ef, tlT, true);
+                    success = true;
+                elseif length(tlT)<length(ef) && ~isempty(tlT)
+                    [~,b,success] = findCorrection(ef, tlT, false);
+                elseif length(tlT)>length(ef) && ~isempty(tlT)
+                    [~,a,success] = findCorrection(tlT, ef, false);
+                    if ~isempty(a)
+                        b = [1/a(1); -a(2)/a(1)];
+                    end
+                end
+                if success
+                    %                 writeNPY(b, fullfile(alignDir, ...
+                    %                     sprintf('correct_timeline_%d_to_ephys_%s.npy', ...
+                    %                     e, tags{1})));
+                    bTL2ephys(:,e) = b;
+                    writeNPY(b, fullfile(alignDir, ...
+                        sprintf('correct_timeline_%d_to_ephys_%s.npy', ...
+                        expNums(e), tags{1})));
+                    fprintf('success\n');
+                    eTimeline2keep = [eTimeline2keep e];
+                else
+                    fprintf('could not correct timeline to ephys\n');
+                end
+            end
+        end
+    end
     
-    fprintf(1, 'loading timeline\n');
-    load(fullfile(server, mouseName, thisDate, num2str(expNum), [thisDate '_' num2str(expNum) '_' mouseName '_Timeline.mat']));
+    %% Load each Timeline/Block pair
     
-    % find the timeline samples where cam sync pulses started (went
-    % from 0 to 5V)
-    tt = Timeline.rawDAQTimestamps;
-    syncIndex = find(strcmp({Timeline.hw.inputs.name}, tlSyncName));
-    tlSync = Timeline.rawDAQData(:,syncIndex);
-    [~, tlSyncOnSamps, ~] = schmittTimes(1:numel(tlSync), tlSync, tlSyncThresh);
+    eBlock2keep = [];
     
-    vidSyncOnFrames = intensDown;
+    for e = 1:length(expNums)
+        if sum(eTimeline2keep == e)>0 && hasTimeline(e) && hasBlock(e) && contains(blocks{e}.expDef,P.expdef)
+            
+            eTL = e; % should be the same
+            if useFlipper
+                % didn't get photodiode flips above, so get them now
+                Timeline = tls{eTL};
+                tt = Timeline.rawDAQTimestamps;
+                evTrace = Timeline.rawDAQData(:, strcmp({Timeline.hw.inputs.name}, 'photoDiode'));
+                pdT = schmittTimes(tt, evTrace, [8 9]);
+                if isempty(pdT)
+                    pdT = schmittTimes(tt, evTrace, [2.5 3]);
+                end
+            else
+                pdT = tlFlips{eTL};
+            end
+%             pdT = pdT(2:end);
+            
+            fprintf('trying to correct block %d to timeline %d\n', expNums(e), expNums(eTL));
+            
+            block = blocks{e};
+            sw = block.stimWindowUpdateTimes;
+            sw = sw(2:end); % sometimes need this? Why? how did sw
+            % get an extra event at the beginning?
+            
+            success = false;
+            if length(sw)<length(pdT) && length(sw)>1
+                [~,b,success,actualTimes] = findCorrection(pdT, sw, false);
+                if ~success
+                    % might be because photodiode acted strangely
+                    % find unexpected photodiode events
+                    unexp_idx = find(diff(pdT) < min(diff(sw)));
+                    % remove them
+                    pdT(unexp_idx(2):unexp_idx(2)+1) = [];
+                    [~,b,success,actualTimes] = findCorrection(pdT, sw, false);
+                end
+            end
+            if length(sw)==length(pdT)
+                % easy case: the two are exactly coextensive
+                [~,b] = makeCorrection(pdT, sw, true);
+                success = true;
+            end
+            if success
+                %                     writeNPY(b, fullfile(alignDir, ...
+                %                         sprintf('correct_block_%d_to_timeline_%d.npy', ...
+                %                         e, eTL)));
+                %                     writeNPY(actualTimes, fullfile(alignDir, ...
+                %                         sprintf('block_%d_sw_in_timeline_%d.npy', ...
+                %                         e, eTL)));
+                writeNPY(b, fullfile(alignDir, ...
+                    sprintf('correct_block_%d_to_timeline_%d.npy', ...
+                    expNums(e), expNums(eTL))));
+                
+                fprintf('  success\n');
+                eBlock2keep = [eBlock2keep e];
+            else
+                fprintf('  could not correct block %d to timeline %d\n', expNums(e), expNums(eTL));
+            end
+        elseif isMpep(e)
+            % take it from Anna's script if needed
+        end
+    end
     
-    A = importdata(fullfile(server, mouseName, thisDate, num2str(expNum), [movieName, '_times.txt']),'\t');
-    timeFoundBetweenSyncs = A.data(vidSyncOnFrames(2),end)-A.data(vidSyncOnFrames(1),end);
-    theoTimeBetweenSyncs = diff(tt(tlSyncOnSamps));
-    
-    timeDiscr = theoTimeBetweenSyncs-timeFoundBetweenSyncs;
-    
-    % time discrepancy could be due to a difference in timing (i.e., linear
-    % scaling of time). Would be great to know.
-    fprintf(1, 'time discrepancy was %ds \n', timeDiscr);
-    
-    % get the actual timestamps for the video in question.
-    % take the metadata
-    a = (tt(tlSyncOnSamps(2)) - tt(tlSyncOnSamps(1)))/(A.data(vidSyncOnFrames(2),end)-A.data(vidSyncOnFrames(1),end));
-    b = tt(tlSyncOnSamps(1)) - a*A.data(vidSyncOnFrames(1),end);
-    tVid = a*A.data(:,end) + b;
-    vidFs = mean(diff(A.data(:,end)));
-    
-    saveName = fullfile(server, mouseName, thisDate, num2str(expNum), ...
-        [movieName '_timeStamps.mat']);
-    fprintf(1, 'saving to %s\n', saveName)
-    
-    save(saveName, 'tVid', 'vidFs');
+    TL = tls(eBlock2keep);
+    BLOCKS = blocks(eBlock2keep);
 end
