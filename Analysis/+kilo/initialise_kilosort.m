62931a3f6bc0c68029fdf68ab8b669d639c7ad00
takacsflora <takacsflora@gmail.com>
2022-01-17 00:42:38 +0000

    change ks

--- Analysis/+kilo/initialise_kilosort.m
+++ Analysis/+kilo/initialise_kilosort.m
@@ -1,13 +1,15 @@
 function []=initialise_kilosort()
 %% deal with paths
-addpath(genpath('C:\Users\Flora\Documents\GitHub\KiloSort2')) % path to kilosort folder
-addpath('C:\Users\Flora\Documents\GitHub\npy-matlab') % for converting to Phy
+addpath(genpath('C:\Users\Experiment\Documents\GitHub\PinkRigs')) % path to kilosort folder
+
+addpath(genpath('C:\Users\Experiment\Documents\GitHub\Kilosort-2.0')) % path to kilosort folder
+addpath(genpath('C:\Users\Experiment\Documents\GitHub\npy-matlab')) % for converting to Phy
 
 csvRoot='\\zserver.cortexlab.net\Code\AVrig\'; % folder in which queue csv resides
-pathToKSConfigFile = 'C:\Users\Flora\Documents\Github\AV_passive\preprocessing\configFiles_kilosort2'; 
-kilosortworkfolder='C:\Users\Flora\Documents\KSworkfolder'; % local folder on ssd where I process the data for whitening (rootH) 
-kilosortoutputfolder='C:\Users\Flora\Documents\kilosort'; % local temporal folder for output (rootZ)
-defaultP3Bchanmap='C:\Users\Flora\Documents\Github\AV_passive\preprocessing\configFiles_kilosort2\neuropixPhase3B2_kilosortChanMap.mat';
+pathToKSConfigFile = 'C:\Users\Experiment\Documents\Github\AV_passive\preprocessing\configFiles_kilosort2'; 
+kilosortworkfolder='C:\Users\Experiment\Documents\KSworkfolder'; % local folder on ssd where I process the data for whitening (rootH) 
+kilosortoutputfolder='C:\Users\Experiment\Documents\kilosort'; % local temporal folder for output (rootZ)
+defaultP3Bchanmap='C:\Users\Experiment\Documents\Github\AV_passive\preprocessing\configFiles_kilosort2\neuropixPhase3B2_kilosortChanMap.mat';
 
 %% the rest 
 % check which days from the mice's folder contain ephys data
@@ -15,9 +17,7 @@ if ~exist(kilosortworkfolder, 'dir')
    mkdir(kilosortworkfolder)
 end
 
-if ~exist(kilosortoutputfolder, 'dir')
-   mkdir(kilosortoutputfolder)
-end
+
 % check active mice
 csvLocation = [csvRoot 'kilosort_queue.csv'];
 csvData = readtable(csvLocation,'Delimiter',',');
@@ -29,9 +29,12 @@ for recidx=1:numel(activeSortQueue)
 % identify parent folder where we will push the output
     myAPdata=[activeSortQueue{1}]; 
     [ephys_folder,b,c]=fileparts(myAPdata); myapbin=strcat(b,c);
-
+    
+    if ~exist(kilosortoutputfolder, 'dir')
+       mkdir(kilosortoutputfolder)
+    end
     % indentify meta file and create channel map
-    meta=ReadMeta_GLX(myAPdata,ephys_folder); 
+    meta=kilo.ReadMeta_GLX(myAPdata,ephys_folder); 
     if contains(meta.imDatPrb_type,'0')
     % phase 3B probe -- just load the default kilosort map
     channelmapdir=defaultP3Bchanmap;
@@ -39,8 +42,8 @@ for recidx=1:numel(activeSortQueue)
     elseif contains(meta.imDatPrb_type,'2')
         % create channelmap (good for all phase2, even single shank) or copy P3B map?    
         fprintf('creating custom channelmap...') 
-        [~]=create_channelmapMultishank(myAPdata,apbinfiles(myprobe).folder,0);        
-        channelmapfile=dir([apbinfiles(myprobe).folder '\\**\\*_channelmap.mat*']);
+        [~]=kilo.create_channelmapMultishank(myAPdata,ephys_folder,0);        
+        channelmapfile=dir([ephys_folder '\\**\\*_channelmap.mat*']);
         channelmapdir=[channelmapfile(1).folder '\' channelmapfile(1).name]; % channelmap for the probe - should be in the same folder
     end
     %%
@@ -53,7 +56,7 @@ for recidx=1:numel(activeSortQueue)
     end
 
     try
-        Kilosort2Matlab(kilosortoutputfolder,kilosortworkfolder,channelmapdir,pathToKSConfigFile)
+        kilo.Kilosort2Matlab(kilosortoutputfolder,kilosortworkfolder,channelmapdir,pathToKSConfigFile)
         delete([kilosortoutputfolder '\' myapbin]); % delete .bin file from KS output
         movefile(kilosortoutputfolder,ephys_folder) % copy KS output back to server
 
@@ -68,11 +71,11 @@ for recidx=1:numel(activeSortQueue)
         end
 
         % overwrite the queue
-        csvData.sortedTag(idx)=1; 
+        csvData.sortedTag(recidx)=1; 
     catch 
         % sorting was not successful write a permanent tag indicating that
-        csvData.sortedTag(idx)=-1;
-        errorMsge=jsonencode(lastwarn);
+        csvData.sortedTag(recidx)=-1;
+        errorMsge=jsonencode(lasterror);
         fid = fopen([ephys_folder '\KSerror.json'], 'w');
         fprintf(fid, '%s', errorMsge);
         fclose(fid);
