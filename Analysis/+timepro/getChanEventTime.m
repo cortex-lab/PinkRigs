4eb9f9181fe1a6ebadea92dd87d77cd4a42eb45a
cbimbo <c.bimbard@ucl.ac.uk>
2022-01-28 15:19:13 +0000

    Global update alignment

--- Analysis/+timepro/getChanEventTime.m
+++ Analysis/+timepro/getChanEventTime.m
@@ -9,25 +9,27 @@ function evTimes = getChanEventTime(timeline,chanName)
     if ~isempty(chan)
         switch chanName
             case 'photoDiode'
-                % Uses k-means here. Different form the shmitt times I used
-                % to use; also takes much longer!
+                % Uses k-means here to get thresholds. 
+                % Different form the shmitt times I used to use; also takes much longer!
+                % pdT = schmittTimes(timelineTime,chan, [2.5 3]);
                 
                 [~, thresh] = kmeans(chan,5);
                 thresh = [min(thresh) + range(thresh)*0.2;  max(thresh) - range(thresh)*0.2];
                 
-                %Find flips based on these thresholds.
+                % Find flips based on these thresholds.
                 photoDiodeFlipOn = sort([strfind(chan'>thresh(1), [0 1]), strfind(chan'>thresh(2), [0 1])]);
                 photoDiodeFlipOff = sort([strfind(chan'<thresh(1), [0 1]), strfind(chan'<thresh(2), [0 1])]);
                 photoDiodeFlips = sort([photoDiodeFlipOn photoDiodeFlipOff]);
                 
-                %Remove cases where two flips in the same direction appear in succession (you can't flip to white twice in a row)
+                % Remove cases where two flips in the same direction appear in succession (you can't flip to white twice in a row)
                 photoDiodeFlips([strfind(ismember(photoDiodeFlips, photoDiodeFlipOn), [1 1])+1 strfind(ismember(photoDiodeFlips, photoDiodeFlipOff), [1 1])+1]) = [];
                 
-                %Get corresponding flip times. Remove any that would be faster than 60Hz (screen refresh rate)
-                evTimes = timelineTime(photoDiodeFlips)';
+                % Get corresponding flip times. Remove any that would be faster than 60Hz (screen refresh rate)
+                evTimes = timelineTime(photoDiodeFlips)'; % in timeline time
                 evTimes(find(diff(evTimes)<(12/1000))+1) = [];
                 
             case 'audioOut'
+                % Get audio Onsets
                 
             case 'camSync'
                 % Get cam Sync events (onset of dark flash)
@@ -42,6 +44,7 @@ function evTimes = getChanEventTime(timeline,chanName)
                 evTimes = timelineTime(evTimes); % in timeline time
                 
             case 'rewardEcho'
+                % Get reward events
                 thresh = max(chan)/2;
                 rewardTrace = chan > thresh;
                 evTimes = strfind(rewardTrace', [0 1])+1;
