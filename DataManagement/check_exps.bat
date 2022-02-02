f0e2afcddc6cfb47dd012b0e21be983d1cde57b6
pipcoen <pipcoen@gmail.com>
2022-02-02 16:21:05 +0000

    Minor changes to batch files

--- DataManagement/check_exps.bat
+++ DataManagement/check_exps.bat
@@ -1,7 +1,7 @@
 @echo off
 
-matlab -nodisplay -nosplash -r "cd('C:\Users\Experiment\Documents\Github\PinkRigs\Data-Management'); checkForNewAVRecordings"
-timeout /t 30
+matlab -nodisplay -nosplash -r "cd('C:\Users\Experiment\Documents\Github\PinkRigs\Data-Management'); checkForNewAVRecordings; exit;"
+timeout /t 60
 cmd "/c activate PinkRigs && python C:\Users\Experiment\Documents\Github\PinkRigs\Data-Management\stageKS.py && /c deactivate"
-timeout /t 30
+timeout /t 60
 "C:\Program Files\MATLAB\R2019a\bin\matlab.exe" -nodisplay -nosplash -r "cd('C:\Users\Experiment\Documents\Github\PinkRigs\Analysis'); kilo.initialise_kilosort"
