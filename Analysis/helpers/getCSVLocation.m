0ea4a07bfd9895dea288e580c3ea88cd25ffa466
cbimbo <c.bimbard@ucl.ac.uk>
2022-02-04 12:07:58 +0000

    minor description update

--- Analysis/helpers/getCSVLocation.m
+++ Analysis/helpers/getCSVLocation.m
@@ -1,6 +1,6 @@
 function csvLocation = getCSVLocation(subject)
     %%% This function will find the location of the CSV for a subject (or
-    %%% the main CSV is input is 'main').
+    %%% the main CSV if input is 'main', or the 'kilosort_queue').
     
     csvPath = '\\zserver.cortexlab.net\Code\AVrig\';
     
