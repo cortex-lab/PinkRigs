406d0165124d970ec68efd4f9e7191964f60b55e
cbimbo <c.bimbard@ucl.ac.uk>
2022-02-04 12:08:11 +0000

    moved to helpers

---
+++ Analysis/helpers/extractSync.m
@@ -0,0 +1,13 @@
+function extractSync(AP_filename, nChansTotal)
+    %%% This function will extract the flipper from the ephys data.
+    
+    d = dir(AP_filename);
+    nSamps = d.bytes/2/nChansTotal;
+    mmf = memmapfile(AP_filename,'Format',{'int16', [nChansTotal nSamps],'x'});
+    
+    disp('extracting sync...');
+    sync = mmf.Data.x(385,:);    
+    
+    % save sync data
+    save(fullfile(d.folder,'sync.mat'),'sync');
+end
