d0b28f1d2d489a21dcc8b0432eccd2f4cb29e6fa
takacsflora <takacsflora@gmail.com>
2022-02-08 19:01:06 +0000

    Updating file/folder names

--- DataManagement/check_exps.bat
+++ DataManagement/check_exps.bat
@@ -1,7 +1,7 @@
 @echo off
 
-matlab -nodisplay -nosplash -r "cd('C:\Users\Experiment\Documents\Github\PinkRigs\Data-Management'); checkForNewAVRecordings; exit;"
+matlab -nodisplay -nosplash -r "cd('C:\Users\Experiment\Documents\Github\PinkRigs\DataManagement'); checkForNewAVRecordings; exit;"
 timeout /t 60
-cmd "/c activate PinkRigs && python C:\Users\Experiment\Documents\Github\PinkRigs\Data-Management\stageKS.py && /c deactivate"
+cmd "/c activate PinkRigs && python C:\Users\Experiment\Documents\Github\PinkRigs\DataManagement\stageKS.py && /c deactivate"
 timeout /t 60
 "C:\Program Files\MATLAB\R2019a\bin\matlab.exe" -nodisplay -nosplash -r "cd('C:\Users\Experiment\Documents\Github\PinkRigs\Analysis'); kilo.main"
