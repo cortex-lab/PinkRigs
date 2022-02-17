1fd7c1e6ef6bae32e5426d949befefe5e10152ad
unknown <sitpakhang@gmail.com>
2022-02-17 22:50:26 +0000

    No update message provided

--- Admin/autoRunOvernight.m
+++ Admin/autoRunOvernight.m
@@ -4,12 +4,25 @@ computerType = getComputerType;
 
 switch lower(computerType)
     case 'time'
+        fprintf('Detected timeline computer... \n')
+        
+        fprintf('Running "copyLocalData2ServerAndDelete"... \n')
         copyLocalData2ServerAndDelete;
+        
     case 'ephys'
+        fprintf('Detected ephys computer... \n')
+        fprintf('Running "copyLocalData2ServerAndDelete"... \n')
         copyLocalData2ServerAndDelete;
+        
+        fprintf('Running "copyEphysData2ServerAndDelete"... \n')
         copyEphysData2ServerAndDelete;
+        
     case 'kilo1'
+        fprintf('Detected kilo1 computer... \n')
+        
+        fprintf('Running "checkForNewAVRecordings"... \n')
         checkForNewAVRecordings;
 end
 
+pause(10);
 exit
