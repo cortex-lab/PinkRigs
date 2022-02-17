d85c85da0d08cf95eac6eda266da792f7c43acc6
pipcoen <pipcoen@gmail.com>
2022-02-17 18:02:01 +0000

    Minor changes

--- New Text Document.bat
+++
@@ -1,8 +0,0 @@
-@ECHO OFF
-SETLOCAL EnableExtensions
-set EXE=matlab.exe
-FOR /F %%x IN ('tasklist /NH /FI "IMAGENAME eq %EXE%"') DO IF /I NOT %%x == %EXE% (
-  echo %EXE% is not Running so will start the microphone listener...
-  start matlab.exe
-)
-PAUSE
