12ebf6ad0ccc6578212a75c0d6894fb3a2b36c0d
pipcoen <pipcoen@gmail.com>
2022-02-17 18:01:37 +0000

    Minor changes

--- universalExpStart.bat
+++ universalExpStart.bat
@@ -4,6 +4,7 @@ echo Free space on D: drive is %FreeSpace:~0,-10%
 
 if not x%COMPUTERNAME:TIME=%==x%COMPUTERNAME% (goto :timelineCompStart)
 if not x%COMPUTERNAME:EPHYS=%==x%COMPUTERNAME% (goto :ephysCompStart)
+goto :endfunction
 
 :timelineCompStart
 echo "Detected timeline computer." 
@@ -19,14 +20,15 @@ if %FreeSpace:~0,-10% leq %GBTest% (
 	echo "Free space less than %GBTest%GB. Delete some files"
 	PAUSE
 )
+goto :endfunction
 
-:timelineCompStart
+:ephysCompStart
 echo "Detected ephys computer." 
 echo "Checking there is enough space on the disk..."
 timeout /T 1 /NOBREAK > nul
 SET /A GBTest = 500
 if %FreeSpace:~0,-10% gtr %GBTest% (
-	echo "Free space more than %GBTest%GB. Opening Timeline"
+	echo "Free space more than %GBTest%GB. Opening SpikeGLX"
         cd C:\Users\Experiment\Documents\SpikeGLX\Release_v20201103-phase30\SpikeGLX 
         start SpikeGLX.exe
 )
@@ -34,5 +36,13 @@ if %FreeSpace:~0,-10% leq %GBTest% (
 	echo "Free space less than %GBTest%GB. Delete some files"
 	PAUSE
 )
+SETLOCAL EnableExtensions
+set EXE=matlab.exe
+FOR /F %%x IN ('tasklist /NH /FI "IMAGENAME eq %EXE%"') DO IF /I NOT %%x == %EXE% (
+  echo %EXE% is not Running so will start the microphone listener...
+  matlab -nodisplay -nosplash -r "cd('D:\Dropbox (Personal)\XMatlabProg\GitHub\PinkRigs\Microphone'); micListener"
+)
+PAUSE
+goto :endfunction
 
-
+:endfunction
