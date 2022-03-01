85d7ab5ccdb642163a5b6b8609f52913a12f2c90
cbimbo <c.bimbard@ucl.ac.uk>
2022-03-01 18:26:00 +0000

    Quick and dirty event extraction

--- Analysis/+preproc/+expDef/AVprotocol.m
+++ Analysis/+preproc/+expDef/AVprotocol.m
@@ -1,19 +1,74 @@
-function ev = AVprotocol(timeline, block, alignment)
+function ev = AVprotocol(timeline, block, alignmentBlock)
     %%% This function will fetch all important information from the AV
     %%% protocols, during postactive or during training.
     
-    %% Extract photodiode onsets in timeline
-    % might need to clean a bit?
+    %     %% Extract photodiode onsets in timeline
+    %     % might need to clean a bit?
+    %
+    %     ev.visStimOnsetTime = timeproc.getChanEventTime(timeline,'photoDiode');
+    %
+    %     %% Extract sounds onsets
+    %     % might need to clean a bit?
+    %
+    %     ev.audStimOnsetTime = timeproc.getChanEventTime(timeline,'audio');
+    %
+    %     %% Extract reward onsets
+    %
+    %     ev.rewardOnsetTimes = timeproc.getChanEventTime(timeline,'reward');
+    %
+    %     %% Extract trial info
     
-    ev.visStimOnsetTime = timeproc.getChanEventTime(timeline,'photoDiode');
+    % QUICK AND DIRTY FOR NOW -- NEED TO CHANGE AND RECOMPUTE
+    % get stim info
+    visTrial = block.events.viscontrastValues > 0;
+    visTrialsLoc = block.events.visazimuthValues; visTrialsLoc(~visTrial) = nan;
+    audTrial = block.events.audamplitudeValues > 0;
+    audTrialsLoc = block.events.audazimuthValues; audTrialsLoc(~audTrial) = nan;
+    rewTrials = block.outputs.rewardValues>0;
     
-    %% Extract sounds onsets
-    % might need to clean a bit?
+    % get stim onsets
+    t = timeproc.extractChan(timeline,'time');
+    pho = timeproc.extractChan(timeline,'photoDiode');
+    aud = timeproc.extractChan(timeline,'audioOut');
     
-    ev.audStimOnsetTime = timeproc.getChanEventTime(timeline,'audio');
+    tmp = block.stimWindowUpdateTimes(2:2:end);
+    [~, phoFlips, ~] = schmittTimes(t, pho, [6 10]);
+    if numel(tmp)>numel(phoFlips)
+        tmp(1) = []; % empirical...
+    end
+
+    stimOnsetRaw = preproc.align.event2Timeline(block.events.visstimONTimes, ...
+        alignmentBlock.originTimes,alignmentBlock.timelineTimes);
     
-    %% Extract reward onsets
+    % get it from photodiode?
+    audOnset = t(find(diff(aud)>1)+1);
+    audOnset(find(diff(audOnset)<1)+1) = [];
+    [~, visOnset, ~] = schmittTimes(t, pho, max(pho)*[0.6 0.9]);
+    visOnset(1) = []; % looks like something else
+    visOnset(find(diff(visOnset)<1)+1) = [];
+    nTrials = numel(block.events.stimuliOnTimes);
+    visOnsetAll = nan(1,nTrials);
+    audOnsetAll = nan(1,nTrials);
+    idxv = 0;
+    idxa = 0;
+    for tt = 1:nTrials
+        if visTrial(tt)
+            idxv = idxv+1;
+            visOnsetAll(tt) = visOnset(idxv);
+        end
+        if audTrial(tt)
+            idxa = idxa+1;
+            audOnsetAll(tt) = audOnset(idxa);
+        end
+    end
+    stimOnset = min([visOnsetAll; audOnsetAll]); % NOT IDEAL AT ALL, TAKES THE FIRST ONE THAT COMES // COULD ALSO TAKE stimOnsetRaw
+    stimOnset(audTrial) = audOnset; % force it to be aligned on auditory onsets for now, because that's what we're interested in...
+    stimOnset(isnan(stimOnset)) = stimOnsetRaw(isnan(stimOnset));
     
-    ev.rewardOnsetTimes = timeproc.getChanEventTime(timeline,'reward');
-    
-    %% Extract trial info
+    %% save it in ev
+    ev.stimOnset = stimOnset;
+    ev.trialInfo.visTrial = visTrial;
+    ev.trialInfo.visTrialsLoc = visTrialsLoc;
+    ev.trialInfo.audTrial = audTrial;
+    ev.trialInfo.audTrialsLoc = audTrialsLoc;
+    ev.trialInfo.rewTrials = rewTrials;
