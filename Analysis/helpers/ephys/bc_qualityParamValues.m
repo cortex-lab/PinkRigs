5b780671d4a7ae59e52f96e1875504fd0d84bf26
takacsflora <c.bimbard@ucl.ac.uk>
2022-03-03 09:38:12 +0000

    Uploaded qmetrics code and params

---
+++ Analysis/helpers/ephys/bc_qualityParamValues.m
@@ -0,0 +1,48 @@
+param = struct;
+
+% added by CB
+param.ksFolder = ksFolder;
+
+%bc_qualityParamValues
+param.plotThis = 0;
+param.plotGlobal = 0;
+% refractory period parameters
+param.tauR = 0.0020; %refractory period time (s)
+param.tauC = 0.0001; %censored period time (s)
+param.maxRPVviolations = 10;
+% percentage spikes missing parameters 
+param.maxPercSpikesMissing = 20;
+param.computeTimeChunks = 0;
+param.deltaTimeChunk = NaN; 
+% number of spikes
+param.minNumSpikes = 300;
+% waveform parameters
+param.maxNPeaks = 2;
+param.maxNTroughs = 1;
+param.somatic = 1; 
+param.minWvDuration = 100; %ms
+param.maxWvDuration = 800; %ms
+param.minSpatialDecaySlope = -20;
+param.maxWvBaselineFraction = 0.3;
+% amplitude parameters
+param.rawFolder = [ephysap_path, '/..'];
+param.nRawSpikesToExtract = 100; 
+param.minAmplitude = 20; 
+% recording parametrs
+param.ephys_sample_rate = 30000;
+param.nChannels = 385;
+% distance metric parameters
+param.computeDistanceMetrics = 0;
+param.nChannelsIsoDist = 4;
+param.isoDmin = NaN;
+param.lratioMin = NaN;
+param.ssMin = NaN; 
+% ACG parameters
+param.ACGbinSize = 0.001;
+param.ACGduration = 1;
+% ISI parameters
+param.longISI = 2;
+% cell classification parameters
+param.propISI = 0.1;
+param.templateDuration = 400;
+param.pss = 40;
