c8fee32517bd3c67a9b71de01a9f5f9caf9f8461
cbimbo <c.bimbard@ucl.ac.uk>
2022-03-14 13:31:07 +0000

    Plot locations of queried experiments

---
+++ Analysis/helpers/ephys/plotRecordingLocations.m
@@ -0,0 +1,124 @@
+clear all
+close all
+
+%% Get experiments
+
+params.mice2Check = 'AV007';
+params.days2Check = 5;
+params.expDef2Check = 'multiSpaceWorld_checker_training';
+params.preproc2Check = '1,*';
+exp2checkList = csv.queryExp(params); 
+
+%% Plot it
+
+% Get probes layout
+mainCSV = csv.readTable(csv.getLocation('main'));
+mouseIdx = strcmpi(mainCSV.Subject,params.mice2Check);
+probeNum = ~isempty(mainCSV(mouseIdx,:).P0_type)+~isempty(mainCSV(mouseIdx,:).P1_type);
+
+% Plot the basic layout
+figure('Position', [680    32   551   964]); hold all
+for pp = 1:probeNum
+    probeType = mainCSV(mouseIdx,:).(sprintf('P%d_type',pp-1)){1};
+    switch probeType
+        case '2.0 - 4shank'
+            % Taken from the IMRO plotting
+            shankNum(pp) = 4;
+            nElec = 1280;   %per shank; pattern repeats for the four shanks
+            vSep = 15;      % in um
+            hSep = 32;
+            shankSep = 250;
+            elecPos = zeros(nElec, 2);
+            elecPos(1:2:end,1) = 0;                %sites 0,2,4...
+            elecPos(2:2:end,1) =  hSep;            %sites 1,3,5...
+            % fill in y values
+            viHalf = (0:(nElec/2-1))';                %row numbers
+            elecPos(1:2:end,2) = viHalf * vSep;       %sites 0,2,4...
+            elecPos(2:2:end,2) = elecPos(1:2:end,2);  %sites 1,3,5...
+        otherwise
+            error('Not yet coded')
+    end
+    
+    for shank = 1:shankNum(pp)
+        scatter((pp-1)*sum(shankNum)*shankSep + shankSep*(shank-1) + elecPos(:,1), elecPos(:,2), 30, 'k', 'square')
+    end
+end
+xlim([-100 3000]);
+ylim([0 10000]);
+%axis equal tight
+
+textLoc = [0 0];
+for ee = 1:size(exp2checkList,1)
+    
+    % Get exp info
+    expInfo = exp2checkList(ee,:);
+    expPath = expInfo.expFolder{1};
+    ephysPaths = regexp(expInfo.ephysRecordingPath,',','split');
+    ephysPaths = ephysPaths{1};
+    
+    for pp = 1:numel(ephysPaths)
+        binFile = dir(fullfile(ephysPaths{pp},'*ap.bin'));
+        if isempty(binFile)
+            fprintf('No bin file in %s\n. Skipping',ephysPaths{pp})
+        else
+            metaData = readMetaData_spikeGLX(binFile.name,binFile.folder);
+            
+            %% Extract info from metadata
+            %%% Same as in plotIMROProtocol
+            out = regexp(metaData.imroTbl,'\(|\)(|\)','split');
+            out(1:2) = []; % empty + extra channel or something?
+            out(end) = []; % empty
+            
+            chans = nan(1,numel(out));
+            shank = nan(1,numel(out));
+            bank = nan(1,numel(out));
+            elecInd = nan(1,numel(out));
+            for c = 1:numel(out)
+                chanProp = regexp(out{c},' ','split');
+                chans(c) = str2double(chanProp{1});
+                shank(c) = str2double(chanProp{2});
+                bank(c) = str2double(chanProp{3});
+                % 4 is refElec
+                elecInd(c) = str2double(chanProp{5});
+            end
+            chanPos = elecPos(elecInd+1,:);
+            
+            for sI = 0:3
+                cc = find(shank == sI);
+                chanPos(cc,1) = chanPos(cc,1) + shankSep*sI;
+            end
+            
+            %% Get protocol color
+            switch expInfo.expDef{1}
+                case 'imageWorld_AllInOne'
+                    protocolColor = [0.7 0.0 0.2];
+                case 'spontaneousActivity'
+                    protocolColor = [0.5 0.5 0.5];
+                case 'multiSpaceWorld_checker_training'
+                    protocolColor = [0.0 0.2 0.8];
+                case 'AVPassive_ckeckerboard_postactive'
+                    protocolColor = [0.0 0.4 0.6];
+                case {'sparseNoise','AP_sparseNoise'}
+                    protocolColor = [0.3 0.3 0.3];
+                otherwise
+                    protocolColor = [0 0 0];
+            end
+            
+            %% Plot a square around parts that were recorded from
+            miXpos = min(chanPos(:,1)) + (pp-1)*sum(shankNum)*shankSep;
+            maXpos = max(chanPos(:,1)) + (pp-1)*sum(shankNum)*shankSep;
+            miDepth = min(chanPos(:,2));
+            maDepth = max(chanPos(:,2));
+            
+            textLoctmp = [maXpos+10,maDepth];
+            while any(all(textLoctmp == textLoc,2)) % Dirty way to avoid overlap
+                textLoctmp = textLoctmp-[0 250];
+            end
+            textLoc = [textLoc; textLoctmp];
+            text(textLoctmp(1),textLoctmp(2),[expInfo.expDate{1} '_' expInfo.expNum{1}],'color',protocolColor)
+            
+            rectangle('Position',[miXpos-ee*5, miDepth, maXpos-miXpos+ee*10, maDepth-miDepth], ...
+                'EdgeColor',protocolColor,'LineWidth',3)
+        end
+    end
+end   
