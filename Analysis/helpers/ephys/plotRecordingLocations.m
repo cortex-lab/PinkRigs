a156a4ee1ba74eba99ff591348c8be12c92af6cf
cbimbo <c.bimbard@ucl.ac.uk>
2022-03-14 15:47:10 +0000

    Update plotRecordingLocations

--- Analysis/helpers/ephys/plotRecordingLocations.m
+++ Analysis/helpers/ephys/plotRecordingLocations.m
@@ -3,10 +3,10 @@ close all
 
 %% Get experiments
 
+clear params
 params.mice2Check = 'AV007';
-params.days2Check = 5;
-params.expDef2Check = 'multiSpaceWorld_checker_training';
-params.preproc2Check = '1,*';
+params.days2Check = 7;
+params.expDef2Check = 'AVPassive_ckeckerboard_postactive';
 exp2checkList = csv.queryExp(params); 
 
 %% Plot it
@@ -24,7 +24,7 @@ for pp = 1:probeNum
         case '2.0 - 4shank'
             % Taken from the IMRO plotting
             shankNum(pp) = 4;
-            nElec = 1280;   %per shank; pattern repeats for the four shanks
+            nElec = 1280;   % per shank; pattern repeats for the four shanks
             vSep = 15;      % in um
             hSep = 32;
             shankSep = 250;
@@ -44,10 +44,11 @@ for pp = 1:probeNum
     end
 end
 xlim([-100 3000]);
-ylim([0 10000]);
+ylim([0 6000]);
 %axis equal tight
 
 textLoc = [0 0];
+boxLoc = [0 0 0 0];
 for ee = 1:size(exp2checkList,1)
     
     % Get exp info
@@ -59,7 +60,7 @@ for ee = 1:size(exp2checkList,1)
     for pp = 1:numel(ephysPaths)
         binFile = dir(fullfile(ephysPaths{pp},'*ap.bin'));
         if isempty(binFile)
-            fprintf('No bin file in %s\n. Skipping',ephysPaths{pp})
+            fprintf('No bin file in %s. Skipping.\n',ephysPaths{pp})
         else
             metaData = readMetaData_spikeGLX(binFile.name,binFile.folder);
             
@@ -88,37 +89,40 @@ for ee = 1:size(exp2checkList,1)
                 chanPos(cc,1) = chanPos(cc,1) + shankSep*sI;
             end
             
-            %% Get protocol color
-            switch expInfo.expDef{1}
-                case 'imageWorld_AllInOne'
-                    protocolColor = [0.7 0.0 0.2];
-                case 'spontaneousActivity'
-                    protocolColor = [0.5 0.5 0.5];
-                case 'multiSpaceWorld_checker_training'
-                    protocolColor = [0.0 0.2 0.8];
-                case 'AVPassive_ckeckerboard_postactive'
-                    protocolColor = [0.0 0.4 0.6];
-                case {'sparseNoise','AP_sparseNoise'}
-                    protocolColor = [0.3 0.3 0.3];
-                otherwise
-                    protocolColor = [0 0 0];
-            end
-            
             %% Plot a square around parts that were recorded from
             miXpos = min(chanPos(:,1)) + (pp-1)*sum(shankNum)*shankSep;
             maXpos = max(chanPos(:,1)) + (pp-1)*sum(shankNum)*shankSep;
             miDepth = min(chanPos(:,2));
             maDepth = max(chanPos(:,2));
             
+            boxLoctmp = [miXpos maXpos miDepth maDepth];
+            spacer = 0;
+            while any(((boxLoctmp(3) >= boxLoc(:,3) & boxLoctmp(3) <= boxLoc(:,4)) | ...
+                    (boxLoctmp(4) >= boxLoc(:,3) & boxLoctmp(4) <= boxLoc(:,4)) | ...
+                    (boxLoctmp(4) >= boxLoc(:,4) & boxLoctmp(3) <= boxLoc(:,3))) & ...
+                    boxLoctmp(1) == boxLoc(:,1))
+                spacer = spacer + 15;
+                boxLoctmp = [miXpos-spacer maXpos+spacer miDepth maDepth];
+            end
+            boxLoc = [boxLoc; boxLoctmp];
+            
             textLoctmp = [maXpos+10,maDepth];
             while any(all(textLoctmp == textLoc,2)) % Dirty way to avoid overlap
-                textLoctmp = textLoctmp-[0 250];
+                textLoctmp = textLoctmp - [0 100];
             end
             textLoc = [textLoc; textLoctmp];
-            text(textLoctmp(1),textLoctmp(2),[expInfo.expDate{1} '_' expInfo.expNum{1}],'color',protocolColor)
+            protocolColor = getProtocolColor(expInfo.expDef{1});
+            text(boxLoctmp(2),textLoctmp(2),[expInfo.expDate{1} '_' expInfo.expNum{1}],'color',protocolColor)
             
-            rectangle('Position',[miXpos-ee*5, miDepth, maXpos-miXpos+ee*10, maDepth-miDepth], ...
-                'EdgeColor',protocolColor,'LineWidth',3)
+            
+            rectangle('Position',[boxLoctmp(1), boxLoctmp(3), boxLoctmp(2)-boxLoctmp(1), boxLoctmp(4)-boxLoctmp(3)], ...
+                'EdgeColor',protocolColor,'FaceColor',[protocolColor 0.1],'LineWidth',3)
         end
     end
 end   
+
+protocols = unique(exp2checkList.expDef);
+for pro = 1:numel(protocols)
+    protocolColor = getProtocolColor(protocols{pro});
+    text(shankNum(1)*shankSep,6000-pro*100,regexprep(protocols{pro},'_',' '),'color',protocolColor)
+end
