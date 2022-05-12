481948b22605b147a85818cea804e0a73df80736
pipcoen <pipcoen@gmail.com>
2022-05-12 12:11:09 +0100

    No update message provided

--- Analysis/helpers/ephys/onlineReceptiveFields/main_RFonline.m
+++ Analysis/helpers/ephys/onlineReceptiveFields/main_RFonline.m
@@ -5,13 +5,13 @@ clc; clear;
 % The depth of SC varies but you can expect it 1-2 mm from surface so select bank accordindly. 
 % 2. Run SparseNoise protocol for 10 mins
 % 
-RID.mname='AV008'; % mouse name 
-RID.date='2022-03-09'; % date
+RID.mname='AV005'; % mouse name 
+RID.date='2022-05-11'; % date
 RID.SparseNoise_expnum=2; % expnum related to sparseNoise experiment
 RID.root=sprintf('\\\\zinu.cortexlab.net\\Subjects\\%s\\%s',RID.mname,RID.date); % where to find block and timeline for sparsenoise 
-RID.ephys_name=sprintf('%s_%s_sparseNoise_Part2_g0',RID.mname,RID.date); % ephys recording name
+RID.ephys_name=sprintf('%s_%s_SparseNoise_Part1_g0',RID.mname,RID.date); % ephys recording name
 %RID.ephys_name=sprintf('FT008_RFmap_shank0_g2');
-RID.probename='imec1';
+RID.probename='imec0';
 RID.ephys_folder=['D:\\ephysData\\' RID.ephys_name]; % local save folder
 %where you can isntantly access ephys data
 %rig 
