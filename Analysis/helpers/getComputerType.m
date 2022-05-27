6f5a6ec6f7e1d40af870f7bab46bed40605e6410
Célian Bimbard <54884591+cbimbo@users.noreply.github.com>
2022-05-27 15:17:07 +0100

    Update getComputerType.m

--- Analysis/helpers/getComputerType.m
+++ Analysis/helpers/getComputerType.m
@@ -12,6 +12,8 @@ elseif contains(lower(computerName), 'mc')
     computerType = 'mc';
 elseif contains(lower(computerName), 'kilo1')
     computerType = 'kilo1';
+elseif contains(lower(computerName), 'kilo2')
+    computerType = 'kilo2';
 else
     warning('Computer type not recognized?!')
     computerType = 'Unrecognized';
