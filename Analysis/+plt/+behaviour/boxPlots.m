f34056c112a1e9bfc9aaf9e6d1d2f09bf90b1656
Pip Coen <Pip@Zippy.ucl.ac.uk>
2022-06-06 10:35:41 +0100

    No update message provided

--- Analysis/+plt/+behaviour/boxPlots.m
+++ Analysis/+plt/+behaviour/boxPlots.m
@@ -1,112 +1,67 @@
-function plotData = boxPlots(subject, expDate, plotType, expDef, opt)
+function plotData = boxPlots(varargin)
 %% Generate box plots for the behaviour of a mouse/mice
-
-% INPUTS (default)
-% subject---Subject(s) to plt. Can be cell array (must provide)
-
-% expDate---expDate(s) to plot (last5)
-    %        'yyyy-mm-dd'--------------A specific date
-    %        'all'---------------------All data
-    %        'lastx'-------------------The last x days of data (especially useful during training)
-    %        'firstx'------------------The first x days of data
-    %        'yest'--------------------The x-1 day, where x is the most
-    %        recent day 
-    %        'yyyy-mm-dd:yyyy-mm-dd'---Load dates in this range (including the boundaries)
-    
-% plotType--Type of box plot ('res')
-    %        'res'---------------------Fraction of rightward responses
-    %        'tim'---------------------Median reaction times for each condition
-    
-% sepPlots--Separate plots by session--only possible if 1 subject (1)
-
-% expDef----Name of required expDef ('multiSpaceWorld_checker_training')
-
 %% Input validation and default assingment
-if exist('opt', 'var')
-    if isfield(opt, 'sepPlots'); sepPlots = opt.sepPlots; end
-    if isfield(opt, 'expNum'); expNum = opt.expNum; end
-    if isfield(opt, 'noPlot'); noPlot = opt.noPlot; end
-end
+opt = csv.inputValidation(varargin{:});
+opt = csv.addDefaultParam(opt, 'noPlot', 0);
+opt = csv.addDefaultParam(opt, 'plotType', 'res');
 
-if ~exist('subject', 'var'); error('Must specify subject'); end
-if ~exist('expDate', 'var'); expDate = 'last5'; end
-if ~exist('expNum', 'var'); expNum = 'any'; end
-if ~exist('plotType', 'var'); plotType = 'res'; end
-if ~exist('expDef', 'var'); expDef = 'multiSpaceWorld_checker_training'; end
-if ~iscell(subject); subject = {subject}; end
-if ~exist('noPlot', 'var'); noPlot = 0; end
-
-nSubjects = length(subject);
-if ~strcmp(expNum, 'any')
-    if ~all([length(expNum) length(expDate)] == nSubjects)
-        error('If requesting expNum, subjects/date/expnum must be the same size');
-    end
-end
-
-if nSubjects > 1 && ~exist('sepPlots', 'var')
+nSubjects = length(opt.subject);
+if nSubjects > 1 && ~isfield(opt, 'sepPlots')
     fprintf('Multiple subjects, so will combine within subjects \n');
-    sepPlots = 0;
-elseif ~exist('sepPlots', 'var')
-    sepPlots = 1;
+    opt = csv.addDefaultParam(opt, 'sepPlots', 0);
+elseif ~isfield(opt, 'sepPlots')
+    opt = csv.addDefaultParam(opt, 'sepPlots', 1);
 end
 
-if ~iscell(expDate); expDate = {expDate}; end
-if ~iscell(expNum); expNum = {expNum}; end
-if ~iscell(expDef); expDef = {expDef}; end
-
-
-if length(expDate)==1 && 1 ~= nSubjects; expDate = repmat(expDate, nSubjects,1); end
-if length(expNum)==1 && 1 ~= nSubjects; expNum = repmat(expNum, nSubjects,1); end
-if length(expDef)==1 && 1 ~= nSubjects; expDef = repmat(expDef, nSubjects,1); end
-
-blkDates = cell(nSubjects,1);
-rigNames = cell(nSubjects,1);
-
+expList = csv.loadData(opt, loadTag='blk');
+expList = expList(cellfun(@isstruct, expList.blkData),:);
 %%
+[blkDates, rigNames] = deal(cell(nSubjects,1));
 for i = 1:nSubjects
-    [blks, extractedDates] = getDataFromDates(subject{i}, expDate{i}, expNum{i}, expDef{1});
-    if length(unique(extractedDates)) ~= length(extractedDates)
-        expDurations = cellfun(@(x) x.duration, blks);
-        [~, ~, uniIdx] = unique(extractedDates);
+    currData = expList(strcmp(expList.subject, opt.subject{i}),:);
+    if length(unique(currData.expDate)) ~= length(currData.expDate)
+        expDurations = cellfun(@str2double, currData.expDuration);
+        [~, ~, uniIdx] = unique(currData.expDate);
         keepIdx = arrayfun(@(x) find(expDurations == max(expDurations(x == uniIdx))), unique(uniIdx));
-        blks = blks(keepIdx);
+        currData = currData(keepIdx,:);
     end
-    blkDates{i} = cellfun(@(x) datestr(x.endDateTime, 'yyyy-mm-dd'), blks, 'uni', 0);
-    rigNames{i} = cellfun(@(x) [upper(x.rigName(1)) x.rigName(end)], blks, 'uni', 0);
-    
+    blks = currData.blkData;
+    blkDates{i} = currData.expDate;
+    rigNames{i} = strrep(currData.rigName, 'zelda-stim', 'Z');
+
     paramSets = cellfun(@(x) x.events.selected_paramsetValues, blks, 'uni', 0);
     AVParams = cellfun(@(x) [x.audInitialAzimuth(x.numRepeats>0) x.visContrast(x.numRepeats>0)], paramSets, 'uni', 0);
     AVParams = cellfun(@(x) unique([x; x*-1], 'rows'), AVParams, 'uni', 0);
-    
+
     eventNames = {'visInitialAzimuth'; 'audInitialAzimuth'; 'repeatNum'; ...
         'visContrast'; 'feedback'; 'correctResponse'};
     taskEvents = extractEventsFromBlock(blks, eventNames);
-    
-    if ~sepPlots
+
+    if ~opt.sepPlots{i}
         [uniParams, ~, uniMode] = unique(cellfun(@(x) num2str(x(:)'), AVParams, 'uni', 0));
         modeIdx = uniMode == mode(uniMode);
         if numel(uniParams) ~= 1
-            fprintf('Multiple param sets detected for %s, using mode \n', subject{i});
+            fprintf('Multiple param sets detected for %s, using mode \n', currData.subject{1});
         end
         names = fieldnames(taskEvents);
         cellData = cellfun(@(f) {vertcat(taskEvents(modeIdx).(f))}, names);
-        
+
         taskEvents = cell2struct(cellData, names);
         taskEvents.nExperiments = sum(modeIdx);
         blkDates{i} = blkDates{i}(modeIdx);
         rigNames{i} = rigNames{i}(modeIdx);
     end
-    
+
     for j = 1:length(taskEvents)
         responseRecorded = double(taskEvents(j).correctResponse).*~(taskEvents(j).feedback==0);
         responseRecorded(taskEvents(j).feedback<0) = -1*(responseRecorded(taskEvents(j).feedback<0));
         responseRecorded = ((responseRecorded>0)+1).*(responseRecorded~=0);
         taskEvents(j).responseRecorded = responseRecorded;
-        
+
         taskEvents(j).visDiff = taskEvents(j).visContrast.*sign(taskEvents(j).visInitialAzimuth);
         taskEvents(j).audDiff = taskEvents(j).audInitialAzimuth;
     end
-    
+
     if i == 1; extractedData = taskEvents;
     else, extractedData(i,1) = taskEvents;
     end
@@ -120,7 +75,7 @@ axesOpt.gapBetweenAxes = [100 40];
 axesOpt.totalNumOfAxes = length(extractedData);
 
 plotData = cell(length(extractedData), 1);
-if ~noPlot; figure; end
+if ~opt.noPlot{1}; figure; end
 for i = 1:length(extractedData)
     if isfield(extractedData(i), 'nExperiments')
         if extractedData(i).nExperiments == 1
@@ -131,20 +86,17 @@ for i = 1:length(extractedData)
     else
         tDat = extractedData(i);
         boxPlot.nExperiments = 1;
-        boxPlot.extraInf = [blkDates{1}{i} ' on ' rigNames{1}{i}];
+        boxPlot.extraInf = [blkDates{i}{1} ' on ' rigNames{i}{1}];
     end
-    
-    if nSubjects == 1
-        boxPlot.subject = subject;
-    else, boxPlot.subject = subject(i);
-    end
-    
+
+    boxPlot.subject = currData.subject{1};
+
     boxPlot.xyValues = {unique(tDat.visDiff)*100; unique(tDat.audDiff)};
     boxPlot.xyLabel = {'AuditoryAzimuth'; 'VisualContrast'};
     boxPlot.axisLimits = [0 1];
     boxPlot.colorMap = plt.general.redBlueMap(64);
-    
-    switch lower(plotType(1:3))
+
+    switch lower(opt.plotType{1}(1:3))
         case 'res'
             timeOuts = extractedData(i).responseRecorded == 0;
             repTrials = timeOuts*0;
@@ -152,15 +104,15 @@ for i = 1:length(extractedData)
             rep1 = find(repNums==1);
             for j = 1:length(extractedData(i).responseRecorded)
                 if repNums(j)~=1 && ~timeOuts(j)
-                   tstIdx = rep1(find(rep1<j,1,'last'));
-                   if sum(~timeOuts(tstIdx:j)) ~= 1
-                       repTrials(j) = 1;
-                   end
+                    tstIdx = rep1(find(rep1<j,1,'last'));
+                    if sum(~timeOuts(tstIdx:j)) ~= 1
+                        repTrials(j) = 1;
+                    end
                 end
             end
 
             tDat = filterStructRows(tDat, ~timeOuts & ~repTrials);
-            
+
             [~,~,vLabel] = unique(tDat.visDiff);
             [~,~,aLabel] = unique(tDat.audDiff);
             boxPlot.plotData = accumarray([aLabel, vLabel],tDat.responseRecorded,[],@mean)-1;
@@ -171,16 +123,16 @@ for i = 1:length(extractedData)
             colorBar.colorDirection = 'normal';
             colorBar.colorYTick = {'0'; '1'};
     end
-    if ~noPlot
+    if ~opt.noPlot{1}
         plt.general.getAxes(axesOpt, i);
         makePlot(boxPlot);
     end
     plotData{i,1} = boxPlot;
 end
-if ~noPlot
+if ~opt.noPlot{1}
     currentAxisPotision = get(gca, 'position');
     figureSize = get(gcf, 'position');
-    
+
     colorBar.handle = colorbar;
     set(colorBar.handle, 'Ticks', get(colorBar.handle, 'Limits'), 'TickLabels', colorBar.colorYTick, 'YDir', colorBar.colorDirection);
     set(gca, 'position', currentAxisPotision);
