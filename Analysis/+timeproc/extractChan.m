146b5342046961e1e1a1f2abab7571ac92c4eace
Pip Coen <Pip@Zippy.ucl.ac.uk>
2022-06-13 16:18:20 +0100

    No update message provided

--- Analysis/+timeproc/extractChan.m
+++ Analysis/+timeproc/extractChan.m
@@ -1,7 +1,7 @@
 function chan = extractChan(timeline,chanName,varargin)
     %%% Will extract specific channel from timeline.
     %%% Additional argument to display or not warning when not found.
-    
+
     if nargin < 3
         dispWaring = 1;
     else
@@ -14,6 +14,13 @@ function chan = extractChan(timeline,chanName,varargin)
         chan = timeline.rawDAQTimestamps;
     else
         chanIndex = find(strcmpi({timeline.hw.inputs.name}, chanName));
+
+        %To deal with changing name from soundOutput to audioOut
+        if isempty(chanIndex) && strcmpi(chanName, 'audioOut')
+            fprintf('No "audioOut... will try soundOutput \n')
+            chanIndex = find(strcmpi({timeline.hw.inputs.name}, 'soundOutput'));
+        end
+
         if ~isempty(chanIndex)
             chan = timeline.rawDAQData(:,chanIndex);
         else
