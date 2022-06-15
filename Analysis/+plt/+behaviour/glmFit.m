1870e921b9eb3d1d3122d3565146217e9d47cba0
Pip Coen <Pip@Zippy.ucl.ac.uk>
2022-06-15 18:40:43 +0100

    No update message provided

--- Analysis/+plt/+behaviour/glmFit.m
+++ Analysis/+plt/+behaviour/glmFit.m
@@ -17,7 +17,7 @@ params = csv.inputValidation(varargin{:});
 axesOpt.totalNumOfAxes = sum(extracted.validSubjects);
 axesOpt.btlrMargins = [80 100 80 40];
 axesOpt.gapBetweenAxes = [100 60];
-axesOpt.numOfRows = min([axesOpt.totalNumOfAxes, 4]);
+axesOpt.numOfRows = max([2 ceil(axesOpt.totalNumOfAxes/4)]);
 axesOpt.figureHWRatio = 1.1;
 
 glmData = cell(length(extracted.data), 1);
