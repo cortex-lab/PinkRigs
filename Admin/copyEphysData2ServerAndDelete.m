1f4fb518e80f1cfb14d7dc70277ad519973ef53b
cbimbo <c.bimbard@ucl.ac.uk>
2022-06-24 19:10:18 +0100

    Update copyEphysData2ServerAndDelete.m

--- Admin/copyEphysData2ServerAndDelete.m
+++ Admin/copyEphysData2ServerAndDelete.m
@@ -81,19 +81,19 @@ allLocalFiles = cell2mat(allLocalFiles);
 allLocalFilePaths = arrayfun(@(x) fullfile(x.folder, x.name), allLocalFiles, 'uni', 0);
 allServerFilePaths = arrayfun(@(x,y) fullfile(y{1}, x.name), allLocalFiles, serverFolders, 'uni', 0);
 
-% % put .cbin files first
-% cbinIdx = find(arrayfun(@(x) contains(x,'.cbin'), allLocalFilePaths));
-% otherIdx = find(arrayfun(@(x) ~contains(x,'.cbin'), allLocalFilePaths));
-% allLocalFilePaths = allLocalFilePaths([cbinIdx; otherIdx]);
-% allServerFilePaths = allServerFilePaths([cbinIdx; otherIdx]);
-% 
-% %Sanity check to make sure that the files are in the correct order
-% allLocalFilePathsTest = cellfun(@(x) x(max(strfind(x, '\'))+1:end), allLocalFilePaths, 'uni', 0);
-% allServerFilePathsTest = cellfun(@(x) x(max(strfind(x, '\'))+1:end), allServerFilePaths, 'uni', 0);
-% matchTest = all(cellfun(@(x,y) strcmp(x,y), allLocalFilePathsTest, allServerFilePathsTest));
-% if ~matchTest
-%     error('File paths names do not correspond..?')
-% end
+% put .cbin files first
+cbinIdx = find(arrayfun(@(x) contains(x,'.cbin'), allLocalFilePaths));
+otherIdx = find(arrayfun(@(x) ~contains(x,'.cbin'), allLocalFilePaths));
+allLocalFilePaths = allLocalFilePaths([cbinIdx; otherIdx]);
+allServerFilePaths = allServerFilePaths([cbinIdx; otherIdx]);
+
+%Sanity check to make sure that the files are in the correct order
+allLocalFilePathsTest = cellfun(@(x) x(max(strfind(x, '\'))+1:end), allLocalFilePaths, 'uni', 0);
+allServerFilePathsTest = cellfun(@(x) x(max(strfind(x, '\'))+1:end), allServerFilePaths, 'uni', 0);
+matchTest = all(cellfun(@(x,y) strcmp(x,y), allLocalFilePathsTest, allServerFilePathsTest));
+if ~matchTest
+    error('File paths names do not correspond..?')
+end
 
 copyFiles2ServerAndDelete(allLocalFilePaths, allServerFilePaths, 1)
 %%
