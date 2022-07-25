ca8bacfa81a293203135e8b1115f5e701e6fd9ec
pipcoen <pipcoen@gmail.com>
2022-07-25 11:06:44 +0100

    No update message provided

--- Analysis/helpers/getServersList.m
+++ Analysis/helpers/getServersList.m
@@ -4,5 +4,5 @@ function servers = getServersList
     servers = { ...
     '\\zaru.cortexlab.net\Subjects\'; ... %1st
     '\\zinu.cortexlab.net\Subjects\'; ... %2nd
-    '\\znas.cortexlab.net\Subjects\'};%; ... %3rd
+    '\\znas.cortexlab.net\Subjects\'; ... %3rd
     '\\zubjects.cortexlab.net\Subjects\'}; %4th
