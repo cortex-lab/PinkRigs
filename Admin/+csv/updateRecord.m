4cca1fd4313bb7fa1ec567c8443f5f0fed347c0f
pipcoen <pipcoen@gmail.com>
2022-07-26 17:08:16 +0100

    No update message provided

--- Admin/+csv/updateRecord.m
+++ Admin/+csv/updateRecord.m
@@ -323,7 +323,7 @@ for pIdx = find(nDat.alignEphys == 1)
         nDat.issortedKS2(pIdx) = 1;
     elseif ~isempty(dir([ephysPath '\KSerror.json']))
         % Issue a "2" if error file is in folder
-        nDat.issortedKS2(pIdx) = 0;
+        nDat.issortedKS2(pIdx) = 2;
     else
         % Issue a "0" if no error, but sorting doesn't exist yet
         nDat.issortedKS2(pIdx) = 1;
@@ -345,7 +345,7 @@ for pIdx = find(nDat.alignEphys == 1)
         nDat.issortedPyKS(pIdx) = 1;
     elseif ~isempty(dir([ephysPath '\KSerror.json']))
         % Issue a "2" if error file is in folder
-        nDat.issortedPyKS(pIdx) = 0;
+        nDat.issortedPyKS(pIdx) = 2;
     else
         % Issue a "0" if no error, but sorting doesn't exist yet
         nDat.issortedPyKS(pIdx) = 1;
