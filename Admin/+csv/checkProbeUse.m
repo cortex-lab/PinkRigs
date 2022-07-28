01a9517989dfd2d7958416ac69c72ac897de4eb3
cbimbo <c.bimbard@ucl.ac.uk>
2022-07-28 18:25:34 +0100

    Use checkProbeUse

--- Admin/+csv/checkProbeUse.m
+++ Admin/+csv/checkProbeUse.m
@@ -15,7 +15,7 @@ if ~exist('queryData','var') || isempty(queryData)
 end
 if ~exist('mkPlt','var');  mkPlt = 0; end
 if ~exist('implantSelect','var');  implantSelect = 'all'; end
-if ~iscell(queryData); queryData = num2cell(queryData,2); end
+if ~iscell(queryData); queryData = num2cell(reshape(queryData,[1, numel(queryData)]),1); end
 
 
 subjects = cell(1,numel(queryData));
