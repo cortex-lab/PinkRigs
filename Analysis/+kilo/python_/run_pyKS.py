64c461c7506a1b0027427e6b1157cc188f872394
Célian Bimbard <40361594+takacsflora@users.noreply.github.com>
2022-08-11 13:09:39 +0100

    adding option to resort

--- Analysis/+kilo/python_/run_pyKS.py
+++ Analysis/+kilo/python_/run_pyKS.py
@@ -18,22 +18,39 @@ from pykilosort import run, add_default_handler, neuropixel_probe_from_metafile
 from check_pyKS_queue import stage_KS_queue
 from pyhelpers import save_error_message
 
-def run_pyKS_single_file(path_to_file,recompute_errored_sorting = False):
+def run_pyKS_single_file(path_to_file,recompute_errored_sorting = False, resort = False, bin_file_extension ='cbin'):
     """
     try running pyKS on a single file 
 
     Parameters: 
-    path_to_file: pathlib.Path to the ap.bin/ap.cbin or ap.meta files
-    recompute_errored_sorting: bool, whether to redo if there was a pyKS_error.json in dir 
+    -------------
+    path_to_file: pathlib.Path 
+        to the ap.bin/ap.cbin or ap.meta files, 
+        implemented this way because sometimes there is both a bin or cbin file ( I think)
+
+    recompute_errored_sorting: bool
+        whether to redo if there was a pyKS_error.json in dir 
+
+    
     Returns: 
-    success: bool, whether the sorting was successful or not. 
+    ------------
+    bool
+        whether the sorting was successful or not. 
 
     """
-    # check if sorting was tried already
-    is_tried = (path_to_file.parent / r'pyKS\pyKS_error.json').is_file()
+    # check if sorting was tried with error already
+    is_errored = (path_to_file.parent / r'pyKS\pyKS_error.json').is_file()
+
+    # check if sorting was already completed 
+    is_sorted = (path_to_file.parent / r'pyKS\output\spike_times.npy').is_file()
+
     success = False
 
-    if (is_tried & recompute_errored_sorting) or not is_tried: 
+    # do sorting if errored and asked to recompute error 
+    # or not sorted and not errored
+    # or sorted, but requested a resort
+
+    if (is_errored & recompute_errored_sorting) or ((not is_sorted) & (not is_errored)) or (is_sorted & resort): 
         try:             
             # read the metadata and get the x- and ypos of channels                     
 
@@ -50,14 +67,21 @@ def run_pyKS_single_file(path_to_file,recompute_errored_sorting = False):
 
             add_default_handler(level='INFO') # print output as the algorithm runs
             # find the compressed file of the same name 
-            input_dir = list((path_to_file.parent).glob('*.cbin'))[0] 
+            get_bin_string = '*.%s' % bin_file_extension
+            input_dir = list((path_to_file.parent).glob(get_bin_string))[0] 
+
             run(input_dir, probe=channel_map, low_memory=True, dir_path = KS_workfolder, output_dir=output_dir / 'output')
             # if there was a previous error message, remove it
-
             err_message_file = output_dir / 'pyKS_error.json'
             
             if err_message_file.is_file(): 
                 err_message_file.unlink()
+
+            # if there was a a previous ibl_format folder, remove it as ibl_format corresponds to the sorting output. 
+            ibl_format_path = output_dir / r'output\ibl_format'
+            if ibl_format_path.is_dir(): 
+                shutil.rmtree(ibl_format_path)
+
             success=True
 
         except:
@@ -130,7 +154,8 @@ def run_pyKS_on_queue(run_for=5.5):
             rec = to_sort_recs.iloc[0]
 
             _ = recheck_queue(overwrite=True,my_ephys_name=rec.ephysName,overwrite_value=.5)
-            input_dir = Path(rec.ephysName)
+            input_dir = Path(rec.ephysName)            
+
             success = run_pyKS_single_file(input_dir,recompute_errored_sorting = True)
             if success:
                 _ = recheck_queue(overwrite=True,my_ephys_name=rec.ephysName,overwrite_value=1)
