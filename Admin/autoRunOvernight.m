1aea9168eba6473030795603bd33d625c2386c84
cbimbo <c.bimbard@ucl.ac.uk>
2022-10-07 11:57:53 +0100

    No update message provided

--- Admin/autoRunOvernight.m
+++ Admin/autoRunOvernight.m
@@ -6,15 +6,15 @@ githubPath = fileparts(fileparts(which('autoRunOvernight.m')));
 switch lower(computerType)
     case 'time'
         fprintf('Detected timeline computer... \n')
-        
+
         fprintf('Running "copyLocalData2ServerAndDelete"... \n')
         copyLocalData2ServerAndDelete('D:\LocalExpData');
-        
+
         fprintf('Running "runFacemap" ... \n')
-        % update environment 
+        % update environment
         eveningFacemapPath = [githubPath '\Analysis\\+vidproc\_facemap\run_facemap.py'];
         [statusFacemap resultFacemap] = system(['activate facemap && ' ...
-             'cd ' githubPath ' && ' ...
+            'cd ' githubPath ' && ' ...
             'conda env update --file facemap_environment.yaml --prune' ' &&' ...
             'python ' eveningFacemapPath ' &&' ...
             'conda deactivate']);
@@ -23,17 +23,17 @@ switch lower(computerType)
         end
 
         disp(resultFacemap);
-        
+
     case 'ephys'
         fprintf('Detected ephys computer... \n')
-        
+
         fprintf('Running "copyLocalData2ServerAndDelete"... \n')
         copyLocalData2ServerAndDelete('D:\LocalExpData');
-        
+
         fprintf('Running "extractLocalSync"... \n')
         extractLocalSync('D:\ephysData');
-        
-        fprintf('Compressing local data... \n')     
+
+        fprintf('Compressing local data... \n')
         compressPath = which('compress_data.py');
         [statusComp,resultComp] = system(['conda activate PinkRigs && ' ...
             'python ' compressPath ' && ' ...
@@ -41,15 +41,15 @@ switch lower(computerType)
         if statusComp > 0
             error('Compressing local data failed.')
         end
-        
+
         fprintf('Running "copyEphysData2ServerAndDelete"... \n')
         copyEphysData2ServerAndDelete('D:\ephysData');
-        
+
         fprintf('Running "runFacemap" ... \n')
-        % update environment 
+        % update environment
         eveningFacemapPath = [githubPath '\Analysis\\+vidproc\_facemap\run_facemap.py'];
         [statusFacemap resultFacemap] = system(['activate facemap && ' ...
-             'cd ' githubPath ' && ' ...
+            'cd ' githubPath ' && ' ...
             'conda env update --file facemap_environment.yaml --prune' ' &&' ...
             'python ' eveningFacemapPath ' &&' ...
             'conda deactivate']);
@@ -60,16 +60,16 @@ switch lower(computerType)
         disp(resultFacemap);
 
     case {'kilo1'}
-      
+
         %%
         fprintf('Detected kilo computer... \n')
         fprintf('Starting now %s... \n',datestr(now))
-        
+
         dbstop if error % temporarily, to debug
-        
+
         fprintf('Running "csv.checkForNewPinkRigRecordings"... \n')
         csv.checkForNewPinkRigRecordings('expDate', 1);
-        
+
         c = clock;
         if c(4) > 20
             fprintf('Update on training... \n')
@@ -80,7 +80,7 @@ switch lower(computerType)
                 saveas(gcf,fullfile('C:\Users\Experiment\Documents\BehaviorFigures',['Behavior_' datestr(datetime('now'),'dd-mm-yyyy') '.png']))
                 close(gcf)
             end
-            
+
             % Check status and send email.
             checkTrainingPath = which('check_training_mice.py');
             [statusTrain,resultTrain] = system(['conda activate PinkRigs && ' ...
@@ -90,10 +90,10 @@ switch lower(computerType)
                 fprintf('Updating on training failed with error "%s".\n', resultTrain)
             end
         end
-        
+
         c = clock;
         if c(4) > 20 || c(4) < 2
-            Kilo_runFor = num2str(2); 
+            Kilo_runFor = num2str(2);
         else
             Kilo_runFor = num2str(5);
         end
@@ -102,18 +102,18 @@ switch lower(computerType)
         fprintf('Running pykilosort on the queue... \n')
         runpyKS = [githubPath '\Analysis\+kilo\python_\run_pyKS.py'];
         [statuspyKS,resultpyKS] = system(['activate pyks2 && ' ...
-        'python ' runpyKS ' ' Kilo_runFor ' && ' ...
-        'conda deactivate']);
+            'python ' runpyKS ' ' Kilo_runFor ' && ' ...
+            'conda deactivate']);
         if statuspyKS > 0
             fprintf('Running pyKS failed... "%s".\n', resultpyKS)
         end
-        
+
         disp(resultpyKS);
 
         fprintf('creating the ibl format... \n')
         checkQueuePath = [githubPath '\Analysis\+kilo\python_\convert_to_ibl_format.py'];
         checkWhichMice = 'all';
-        whichKS = 'pyKS'; 
+        whichKS = 'pyKS';
         checkWhichDates = 'last300';
         [statusIBL,resultIBL] = system(['activate iblenv && ' ...
             'python ' checkQueuePath ' ' checkWhichMice ' ' whichKS ' ' checkWhichDates ' && ' ...
@@ -129,84 +129,84 @@ switch lower(computerType)
             %%% that have been aligned but not preprocessed... Have to fix
             %%% it! Have to wait until it's a 0 and not a NaN when ephys
             %%% hasn't been aligned...
-            
+
             fprintf('Running preprocessing...\n')
-            
+
             % Alignment
             preproc.align.main('expDate', 7, 'checkAlignAny', '0')
-            
+
             % Extracting data
             preproc.extractExpData('expDate', 7, 'checkSpikes', '0')
         end
-        
+
 
     case {'kilo2'}
         fprintf('Detected kilo2 computer... \n')
         fprintf('Starting now %s... \n',datestr(now))
-        
-        
+
+
         c = clock;
         if c(4) > 20 || c(4) < 2
-            Kilo_runFor = num2str(2); 
+            Kilo_runFor = num2str(2);
         else
             Kilo_runFor = num2str(5);
         end
 
-        
+
         dbstop if error % temporarily, to debug
         fprintf('Running pykilosort on the queue... \n')
         githubPath = fileparts(fileparts(which('autoRunOvernight.m')));
         runpyKS = [githubPath '\Analysis\+kilo\python_\run_pyKS.py'];
         [statuspyKS,resultpyKS] = system(['activate pyks2 && ' ...
-        'python ' runpyKS ' ' Kilo_runFor ' && ' ...
-        'conda deactivate']);
+            'python ' runpyKS ' ' Kilo_runFor ' && ' ...
+            'conda deactivate']);
         if statuspyKS > 0
             fprintf('Running pyKS failed... "%s".\n', resultpyKS)
         end
-        
+
         disp(resultpyKS);
 
     case {'celians'}
         fprintf('Detected kilo2 computer... \n')
         fprintf('Starting now %s... \n',datestr(now))
-        
-        
+
+
         c = clock;
         if c(4) > 20 || c(4) < 2
-            Kilo_runFor = num2str(2); 
+            Kilo_runFor = num2str(2);
         else
             Kilo_runFor = num2str(5);
         end
 
-        
+
         dbstop if error % temporarily, to debug
         fprintf('Running pykilosort on the queue... \n')
         runpyKS = 'Analysis\+kilo\python_\run_pyKS.py';
         [statuspyKS,resultpyKS] = system(['activate pyks2 && ' ...
             'cd C:\Users\Hamish\OneDrive - University College London\Documents\GitHub\PinkRigs &&' ...
-        'python ' runpyKS ' ' Kilo_runFor ' && ' ...
-        'conda deactivate']);
+            'python ' runpyKS ' ' Kilo_runFor ' && ' ...
+            'conda deactivate']);
         if statuspyKS > 0
             fprintf('Running pyKS failed... "%s".\n', resultpyKS)
         end
-        
+
         disp(resultpyKS);
 
-%         fprintf('creating the ibl format... \n')
-%         checkQueuePath = [githubPath '\Analysis\+kilo\python_\convert_to_ibl_format.py'];
-%         checkWhichMice = 'allActive';
-%         whichKS = 'pyKS'; 
-%         checkWhichDates = 'last7';
-%         [statusIBL,resultIBL] = system(['activate iblenv && ' ...
-%             'python ' checkQueuePath ' ' checkWhichMice ' ' whichKS ' ' checkWhichDates ' && ' ...
-%             'conda deactivate']);
-%         if statusIBL > 0
-%             fprintf('Updating the queue failed with error "%s".\n', resultIBL)
-%         end
-%         disp(resultIBL);
-%         fprintf('Stopping now %s. \n',datestr(now))
+        %         fprintf('creating the ibl format... \n')
+        %         checkQueuePath = [githubPath '\Analysis\+kilo\python_\convert_to_ibl_format.py'];
+        %         checkWhichMice = 'allActive';
+        %         whichKS = 'pyKS';
+        %         checkWhichDates = 'last7';
+        %         [statusIBL,resultIBL] = system(['activate iblenv && ' ...
+        %             'python ' checkQueuePath ' ' checkWhichMice ' ' whichKS ' ' checkWhichDates ' && ' ...
+        %             'conda deactivate']);
+        %         if statusIBL > 0
+        %             fprintf('Updating the queue failed with error "%s".\n', resultIBL)
+        %         end
+        %         disp(resultIBL);
+        %         fprintf('Stopping now %s. \n',datestr(now))
 
 
-        end
+end
 
 end
