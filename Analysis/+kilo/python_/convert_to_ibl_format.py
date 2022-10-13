dfbc851bca100072c508292335e90dac55678992
Célian Bimbard <40361594+takacsflora@users.noreply.github.com>
2022-10-13 14:38:13 +0100

    csv path issue

--- Analysis/+kilo/python_/convert_to_ibl_format.py
+++ Analysis/+kilo/python_/convert_to_ibl_format.py
@@ -185,8 +185,8 @@ def ks_to_ibl_format(ephys_path,ks_folder='pyKS',recompute=False):
     
 def run_batch_ibl_formatting(run_for=2):
     # get queue
-    root = get_server_location / 'Helpers'
-    queue_csv_file = '%s\ibl_formatting_queue.csv' % root
+    root = get_server_location() / 'Helpers'
+    queue_csv_file = root / 'ibl_formatting_queue.csv' 
     queue_csv = pd.read_csv(queue_csv_file)
     print('checking the queue...')
 
