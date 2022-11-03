4ff90aedb3a7de6c6b989261261f233d7e0d1cb7
cbimbo <c.bimbard@ucl.ac.uk>
2022-11-03 10:09:51 +0000

    Update SfN2022_ChronicImplant.m

--- Analysis/figures/SfN2022_ChronicImplant.m
+++ Analysis/figures/SfN2022_ChronicImplant.m
@@ -25,9 +25,10 @@ hold all
 % Find those that match location
 % Do it by subject and probe so that it's easier to do the whole probe
 % thing...?
+pltIndiv = 0;
 recLocSlope = cell(1,1);
 b = cell(1,1);
-useNum = nan(numel(subjects),numel(probes));
+useNum = nan(numel(subjects),2);
 for ss = 1:numel(subjects)
     subjectIdx = contains(subjectsAll,subjects(ss));
     probes = unique(probeSNAll(subjectIdx));
@@ -53,7 +54,7 @@ for ss = 1:numel(subjects)
                 X = [ones(numel(recIdx),1), days(recIdx)'];
                 b{ss,pp}(rr,:) = (X\log10(clusterNum(recIdx)'));
 
-                if params.pltIndiv{1}
+                if pltIndiv
                     plot(days(recIdx), clusterNum(recIdx),'-','color',[colAni(ss,:) .2])
                     scatter(days(recIdx), clusterNum(recIdx),5,colAni(ss,:),'filled','MarkerEdgeAlpha',0.2,'MarkerFaceAlpha',0.2)
                     plot(days(recIdx), 10.^(X*b{ss,pp}(rr,:)'), 'color',colAni(ss,:),'LineWidth',1)
@@ -116,9 +117,128 @@ xlabel('Experiment')
 %% ------ Natural images plots ------
 %% Get data
 
-[dball, res] = natim.main;
-save('\\zserver\Lab\Share\Celian\dataForSfn2022_ChronicImplant_natIm','dball', 'res')
+mice = csv.readTable(csv.getLocation('main'));
+[dball, res] = natim.main(subject=mice(contains(mice.Subject, 'CB'),:).Subject);
+save('\\zserver\Lab\Share\Celian\dataForSfn2022_ChronicImplant_natIm','dball', 'res','-v7.3')
 
 load('\\zserver\Lab\Share\Celian\dataForSfn2022_ChronicImplant_natIm')
 
 %% Plot it
+
+%% Get natural images
+imgDir = '\\zserver.cortexlab.net\Data\pregenerated_textures\Anna\selection112';
+imID = 1;
+im = load(fullfile(imgDir, sprintf('img%d.mat',imID)));
+figure;
+imagesc(im.img)
+colormap(gray)
+axis equal tight
+
+%% Plot stability
+colAni = lines(numel(subjects));
+figure;
+hold all
+for rr = 1:numel(recLocUni)
+    subjectIdx = strcmp(subjects,subjectsAll{rr});
+    plot(idi{rr},NstableDur{rr},'-','color',[colAni(subjectIdx,:)])
+    scatter(idi{rr},NstableDur{rr},10,[colAni(subjectIdx,:)],'filled')
+end
+ylabel({'Number of'; 'matched clusters'})
+xlabel('Number of days between recordings')
+set(gca, 'YScale', 'log')
+% xlim([0.5 7])
+% xticks([1 7])
+% xticklabels([1 7])
+ylim([1 1000])
+yticks([1 10 100 1000])
+yticklabels([1 10 100 1000])
+
+figure;
+subplot(311); hold all
+for rr = 1:numel(recLocUni)
+    subjectIdx = strcmp(subjects,subjectsAll{rr});
+    plot(idi{rr},PstableDur{rr}*100,'-','color',[colAni(subjectIdx,:) .2])
+    scatter(idi{rr},PstableDur{rr}*100,5,[colAni(subjectIdx,:)],'filled','MarkerEdgeAlpha',0.2,'MarkerFaceAlpha',0.2)
+
+    notnan = ~isnan(PstableDur{rr}); notnan(1) = 0;
+    X = [ones(numel(idi{rr}(notnan)),1), log10(idi{rr}(notnan))];
+    b = (X\PstableDur{rr}(notnan)'*100);
+    plot(idi{rr}(2:end), X*b, 'color',colAni(subjectIdx,:),'LineWidth',2)
+    text(idi{rr}(end), X(end,:)*b,[subjects(subjectIdx) '-' recLocUni{rr}],'color',colAni(subjectIdx,:))
+end
+ylim([0 100])
+xticks([1 5 10 20 30])
+xticklabels([1 5 10 20 30])
+xlim([1,30])
+ylabel({'Proportion of'; 'matched clusters'})
+xlabel('Number of days between recordings')
+
+subplot(312); hold all
+for rr = 1:numel(recLocUni)
+    plot(idi{rr},sigCorrStructDur{rr},'-','color',[colAni(strcmp(subjects,subjectsAll{rr}),:) .2])
+    scatter(idi{rr},sigCorrStructDur{rr},5,colAni(strcmp(subjects,subjectsAll{rr}),:),'filled','MarkerEdgeAlpha',0.2,'MarkerFaceAlpha',0.2)
+
+    notnan = ~isnan(sigCorrStructDur{rr}); notnan(1) = 0;
+    X = [ones(numel(idi{rr}(notnan)),1), idi{rr}(notnan)];
+    b = (X\sigCorrStructDur{rr}(notnan)');
+    plot(idi{rr}(notnan), X*b, 'color',colAni(strcmp(subjects,subjectsAll{rr}),:),'LineWidth',2)
+end
+ylim([0 1])
+xticks([1 5 10 20 30])
+xticklabels([1 5 20 30])
+xlim([1,30])
+ylabel({'Stability of' ; 'signal correlations'})
+xlabel('Number of days between recordings')
+
+subplot(313); hold all
+for rr = 1:numel(recLocUni)
+    plot(idi{rr},noiseCorrStructDur{rr},'-','color',[colAni(strcmp(subjects,subjectsAll{rr}),:) .2])
+    scatter(idi{rr},noiseCorrStructDur{rr},5,colAni(strcmp(subjects,subjectsAll{rr}),:),'filled','MarkerEdgeAlpha',0.2,'MarkerFaceAlpha',0.2)
+
+    notnan = ~isnan(noiseCorrStructDur{rr}); notnan(1) = 0;
+    X = [ones(numel(idi{rr}(notnan)),1), idi{rr}(notnan)];
+    b = (X\noiseCorrStructDur{rr}(notnan)');
+    plot(idi{rr}(notnan), X*b, 'color',colAni(strcmp(subjects,subjectsAll{rr}),:),'LineWidth',2)
+end
+ylim([0 1])
+xticks([1 5 10 20 30])
+xticklabels([1 5 10 20 30])
+xlim([1,30])
+ylabel({'Stability of' ; 'noise correlations'})
+
+%% Plot correlation
+
+expIdx2Keep = find(strcmp({dball.recLoc},recLocUni{1}));
+db = dball(expIdx2Keep);
+
+for dd = 1:numel(db)
+    s = size(db(dd).spikeData);
+    resp1 = reshape(nanmean(db(dd).spikeData(:,:,:,1:2:end),[1 4]), [s(2),s(3)]);
+    resp2 = reshape(nanmean(db(dd).spikeData(:,:,:,2:2:end),[1 4]), [s(2),s(3)]);
+    reliability = diag(corr(resp1,resp2));
+
+    reliableUnits = reliability>0.3;
+    db(dd).spikeData = db(dd).spikeData(:,:,reliableUnits,:);
+    db(dd).C.XPos = db(dd).C.XPos(reliableUnits);
+    db(dd).C.Depth = db(dd).C.Depth(reliableUnits);
+    db(dd).C.CluID = db(dd).C.CluID(reliableUnits);
+    db(dd).C.CluLab = db(dd).C.CluLab(reliableUnits);
+end
+
+% Perform CCA
+[corrWCCA, ~] = natim.computeCCA({db.spikeData});
+
+% Get the best matches
+[BestMatch, BestCorr] = natim.getBestMatch(corrWCCA);
+
+% Compute and correct for drift
+Depth = cellfun(@(x) x.Depth, {db.C}, 'uni', 0);
+DepthCorrected = natim.correctDrift(BestMatch, BestCorr, Depth);
+for i = 1:numel(db); db(i).C.DepthCorrected = DepthCorrected{i}; end
+
+% Get distance between clusters
+XPos = cellfun(@(x) x.XPos, {db.C}, 'uni', 0);
+BestDist = natim.getBestDist(BestMatch, XPos, DepthCorrected);
+
+[pairAcrossAll_fewDays,sigCorr,noiseCorr] = natim.plotMatchedNeuronsAcrossDays(1:5, BestMatch, BestCorr, BestDist, ...
+        {db.spikeData}, XPos, DepthCorrected, [db.days],[0.05 0.5 150]);
