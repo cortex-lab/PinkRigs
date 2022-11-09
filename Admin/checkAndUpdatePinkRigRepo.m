fa1f8ee7e5fe1cece77fc0eb31dd65adf72e3da7
pipcoen <pipcoen@gmail.com>
2022-11-09 11:37:29 +0000

    TestingCode

--- Admin/checkAndUpdatePinkRigRepo.m
+++ Admin/checkAndUpdatePinkRigRepo.m
@@ -1,6 +1,7 @@
 function checkAndUpdatePinkRigRepo
     %% Function to check and update the local copy of the PinkRig repo
     % Go to the pink rig repo folder and get git status
+%     Test
     startFolder = cd;
     cd(fileparts(which('zeldaStartup')));
     [~, ~] = system('git remote update');
