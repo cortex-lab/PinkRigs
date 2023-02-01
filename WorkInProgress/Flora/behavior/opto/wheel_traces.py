fc6e6ddab2813d45b5e403803470a14a08afc3c4
takacsflora <40361594+takacsflora@users.noreply.github.com>
2023-02-01 00:18:29 +0000

    plot wheel traces

--- WorkInProgress/Flora/behavior/opto/wheel_traces.py
+++ WorkInProgress/Flora/behavior/opto/wheel_traces.py
@@ -8,21 +8,42 @@ sys.path.insert(0, (pinkRig_path.__str__()))
 
 import matplotlib.pyplot as plt
 import numpy as np
-
-from Admin.csv_queryExp import load_data
 from Analysis.pyutils.wheel_dat import wheel_raster
 
 
-subject = 'AV031'
+from opto_utils import query_opto
+
 data_dict = {
             'events':{'_av_trials':'all'}
                 }
-recordings = load_data(subject = subject, expDate = '2022-12-12',expDef = 'multiSpace',data_name_dict=data_dict)
-# 
-ev = recordings.iloc[0].events['_av_trials']
+
+recordings = query_opto(power=30,hemi = 'R',subject='AV033',expDate = 'all',expDef='multiSpace',data_dict=data_dict)
+recordings = recordings[recordings.extractEvents=='1']
 
 
-my_wheel = wheel_raster(ev,selected_trials=(ev.is_laserTrial & ev.is_validTrial),align_type='laser')
+def batch_rasters(ev): 
+    # sort the indices
+    idxs = np.where(
+        ev.is_laserTrial &
+        ev.is_validTrial &
+        ~np.isnan(ev.timeline_choiceMoveOn)
+        )[0]
 
+    my_wheel = wheel_raster(
+        ev,
+        selected_trials=idxs, 
+        align_type='laser',
+        t = [-0.1,.3]
+        )
+    # sort the raster
+    stim_laser_timediff = (ev.timeline_audPeriodOn[idxs] 
+                                - ev.timeline_laserOn_rampStart[idxs])
 
+    return my_wheel.rasters,stim_laser_timediff
+
+r,td = zip(*[batch_rasters(rec.events._av_trials) for _,rec in recordings.iterrows()])
+rasters = np.concatenate(r)
+td = np.concatenate(td)
+# %%
+plt.imshow(np.abs(rasters[np.argsort(td),:]),aspect='auto',vmin=0,vmax=5)
 # %%
