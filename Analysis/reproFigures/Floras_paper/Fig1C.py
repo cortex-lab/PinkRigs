9c8c3096fab7003cf811f02b4bbd576f36f2c731
takacsflora <40361594+takacsflora@users.noreply.github.com>
2023-02-02 23:31:03 +0000

    update env

--- Analysis/reproFigures/Floras_paper/Fig1C.py
+++ Analysis/reproFigures/Floras_paper/Fig1C.py
@@ -2,24 +2,29 @@
 # general loading functions
 # %%
 import sys
+sys.path.insert(0, r"C:\Users\Flora\Documents\Github\PinkRigs") 
 from pathlib import Path
 import pandas as pd
-sys.path.insert(0, r"C:\Users\Flora\Documents\Github\PinkRigs") 
+import numpy as np
+
 
 from Analysis.pyutils.batch_data import get_data_bunch
-
-dat_keys = get_data_bunch('naive')
+dat_keys = get_data_bunch('naive-allen')
 
 rerun_sig_test=False 
 interim_data_folder = Path(r'C:\Users\Flora\Documents\Processed data\Audiovisual')
-# %%
-
 from Admin.csv_queryExp import load_ephys_independent_probes
 
-
 # %%
+
+allen_pos, curated_label = [],[]
+is_aud_sig, is_vis_sig = [],[]
+
 for _,session in dat_keys.iterrows():
-    r = load_ephys_independent_probes(ephys_dict={'spikes':'all','clusters':'all'},**session)
+    r = load_ephys_independent_probes(ephys_dict={'clusters':'all'},**session)
+    allen_pos.append(r.iloc[0].ibl.clusters.mlapdv)
+    curated_label.append(r.iloc[0].probe.clusters._av_KSLabels)
+
     interim_data_sess = interim_data_folder / ('%s/%s/%.0f/%s/sig_test' % tuple(session))
     interim_data_sess.mkdir(parents=True,exist_ok=True)
     interim_data_sess = interim_data_sess / ('%s_%s_%.0f_%s_maxtest.csv' % tuple(session))
@@ -35,22 +40,26 @@ for _,session in dat_keys.iterrows():
         ) # still, rather slow
     else: 
         p = pd.read_csv(interim_data_sess)
-
     # for each max test get neurons that pass threshold
     bonferroni_p_thr = 0.01/p.columns.size
     is_signifiant_per_cond = p<bonferroni_p_thr
     aud_keys = [k for k in p.keys() if 'aud' in k]
     vis_keys = [k for k in p.keys() if 'vis' in k]
-    is_aud_sig = is_signifiant_per_cond[aud_keys].any(axis=1).to_numpy()
-    is_vis_sig = is_signifiant_per_cond[vis_keys].any(axis=1).to_numpy()
+    is_aud_sig.append(is_signifiant_per_cond[aud_keys].any(axis=1).to_numpy())
+    is_vis_sig.append(is_signifiant_per_cond[vis_keys].any(axis=1).to_numpy())
 
-    is_both = is_aud_sig & is_vis_sig
-    is_neither = ~is_aud_sig & ~is_vis_sig
-    is_aud= is_aud_sig & ~is_vis_sig
-    is_vis= ~is_aud_sig & is_vis_sig
+allen_pos,curated_label = np.concatenate(allen_pos),np.concatenate(curated_label)
+is_aud_sig, is_vis_sig = np.concatenate(is_aud_sig),np.concatenate(is_vis_sig)
 
-    # load cluster location in allen ccf and unit quality metrics
+# %%
+
+# plot the neurons in allen atalas space
 
 
 
 # %%
+
+is_both = is_aud_sig & is_vis_sig
+is_neither = ~is_aud_sig & ~is_vis_sig
+is_aud= is_aud_sig & ~is_vis_sig
+is_vis= ~is_aud_sig & is_vis_sig
