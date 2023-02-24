5fe2df90340b03752f74660c61ee404f2e6ea5a8
takacsflora <40361594+takacsflora@users.noreply.github.com>
2023-02-24 09:53:59 +0000

    adding seaborn,plotly etc

--- Analysis/neural/src/azimuthal_tuning.py
+++ Analysis/neural/src/azimuthal_tuning.py
@@ -1,34 +1,57 @@
 
-import sys,itertools, re
+import sys,itertools, re,time
 import pandas as pd 
 import numpy as np 
 import xarray as xr
 
-from Admin.csv_queryExp import load_ephys_independent_probes,simplify_recdat
+from Admin.csv_queryExp import load_ephys_independent_probes,Bunch
 from Analysis.neural.utils.ev_dat import postactive
 from Analysis.neural.utils.spike_dat import get_binned_rasters
 
 # load data
 
-def get_test_statistic(tuning_curves,tuning_type = None):
+def get_discriminability(tuning_curves,tuning_type = None):
     """
-    utility function to get test statistic from the tuning curve table outputted by the class 
-    this is is specific for the permuation test I am doing and thus requires the tuning 
-    curve table to be called as above 
-
+    utility function to get distriminability from responses at various azimuthal tunings
+    
+    Parameters: 
+    -----------
+    tuning curves: pd.df
+    
+    tuning_type: str for indexing into the tuning curves
+    (so func written for the azimuthal tuning object)
     """ 
     if not tuning_type:
         tuning_type = tuning_curves.columns[0][:3]
-                   
-    train = tuning_curves[(tuning_curves.cv_number == 0)]
-    train = train[('preferred_%s_tuning' % tuning_type)]
-    test = tuning_curves[(tuning_curves.cv_number == 1)]
-    test = test[('preferred_%s_tuning' % tuning_type)]
 
-    statistic = np.abs(train.values.astype('float')-test.values.astype('float'))
+    keys = [c for c in tuning_curves.columns if tuning_type in c and 'preferred' not in c]               
+    resp_per_azimuth = tuning_curves[keys]
+    disriminability = np.ptp(resp_per_azimuth,axis=1)/(np.min(resp_per_azimuth,axis=1)+np.max(resp_per_azimuth,axis=1))
 
-    return statistic
+    return disriminability
 
+
+def get_tc_correlations(tuning_curves):
+    """
+    function to compare how tuning curves are correlated after cv splits
+    requires input with minimum two cv_splits
+
+    Parameters
+    ------------
+
+    """
+    tuning_type = tuning_curves.columns[0][:3]
+    splits = np.unique(tuning_curves.cv_number)
+    keys = [c for c in tuning_curves.columns if tuning_type in c and 'preferred' not in c]
+    # hold out one set for training
+    train = tuning_curves[(tuning_curves.cv_number==0)]
+    train = train[keys]
+    # correlate with other sets 
+    splits = splits[splits>0]
+    corrs = [train.corrwith(tuning_curves[(tuning_curves.cv_number==s)][keys],axis=1) for s in splits]
+    all_corrs = pd.concat(corrs,axis=1)
+    mean_corr = all_corrs.mean(axis=1).values
+    return mean_corr
 
 class azimuthal_tuning():
     def __init__(self,rec_info):
@@ -41,9 +64,10 @@ class azimuthal_tuning():
                 'baseline_subtract': True, 
         }
 
-        self.load(**rec_info)
 
-    def load(self, **rec_info):
+        self.load(rec_info)
+
+    def load(self,rec_info):
 
         ephys_dict =  {'spikes': ['times', 'clusters']}
         other_ = {'events': {'_av_trials': 'table'}}
@@ -56,10 +80,9 @@ class azimuthal_tuning():
         events,self.spikes = recordings.events._av_trials,recordings.probe.spikes
         _,self.vis,self.aud,_ = postactive(events)
 
-
-    def get_tuning_curves(self,contrast = None, spl = None, which = 'vis', subselect_neurons = None,cv_split = 1,azimuth_shuffle=None):
+    def get_rasters_perAzi(self,contrast = None, spl = None, which = 'vis', subselect_neurons = None):
         """
-        get tuning curves for the loaded data
+        get rasters per azimuth for the loaded data
 
         Parameters: 
         ----------
@@ -68,7 +91,7 @@ class azimuthal_tuning():
         which: str
             'vis' or 'aud'
         subselect_neurons: list
-            cluster_ids
+            cluster_ids subselection not implemented. 
         cv_split: float
             no of splits for cross_validation
         
@@ -84,14 +107,9 @@ class azimuthal_tuning():
             spl = np.min(self.aud.SPL.values) 
         
         if not subselect_neurons: 
-            clus_ids = np.unique(self.spikes.clusters)    
-
-        # create indices for cross-validation
-        trials_idx = self.aud.trials.values
-        np.random.seed(0)
-        trials_idx = np.random.permutation(trials_idx)
-        split_edges = list(itertools.accumulate(itertools.repeat(int(trials_idx.size/cv_split),cv_split)))
-        split_edges.insert(0,0)
+            self.clus_ids = np.unique(self.spikes.clusters)    
+        else:
+            pass
 
         if 'vis' in which:
             azimuth_options = sorted(self.vis.azimuths.values)
@@ -99,47 +117,109 @@ class azimuthal_tuning():
         elif 'aud' in which:
             azimuth_options = sorted(self.aud.azimuths.values)
             onset_matrix = self.aud.sel(SPL=spl,timeID='ontimes')      
+                           
+        azimuth_times_dict = {}
+        for azimuth in azimuth_options:
+            azimuth_times_dict[('%s_%.0f' % (which,azimuth))] = onset_matrix.sel(
+                azimuths = azimuth
+            ).values
 
-        if azimuth_shuffle:
-            # shuffle the onset matrix. (eqivalent to shuffling azimuth labels)   
-            np.random.seed(azimuth_shuffle)  # seed ID of the azimuthal shuffing
-            permuted_onset_index = np.random.permutation(np.ravel(onset_matrix))
-            onset_matrix = xr.DataArray(permuted_onset_index.reshape(onset_matrix.shape),
-                                dims=('azimuths','trials'),
-                                coords={'azimuths':azimuth_options})
+        response_rasters = []
+        for k in azimuth_times_dict.keys():
+            t_on = azimuth_times_dict[k]
+            r = get_binned_rasters(self.spikes.times,self.spikes.clusters,self.clus_ids,t_on,**self.raster_kwargs)
+            # so we need to break here so that we perorm getting binned rasters only once...
+            stim = r.rasters[:,:,r.tscale>=0]
+            response_rasters.append(stim[np.newaxis,:,:,:])       
+        
+        self.response_rasters_per_azimuth = Bunch({
+            'dat':np.concatenate(response_rasters,axis=0), 
+            'azimuthIDs':list(azimuth_times_dict.keys())
+            })
+    
 
-            
+
+    def get_tuning_curves(self,rasters = None,cv_split=2,azimuth_shuffle_seed=None):
+        """
+        Parameters: 
+        -----------
+        rasters: np.ndarray 
+            azimuths x trials x neurons x timebins 
+        """
+        if not rasters:
+            rasters = self.response_rasters_per_azimuth.dat
+            azimuths = self.response_rasters_per_azimuth.azimuthIDs
+        else:
+            azimuths = range(rasters.shape[0])
+
+        azimuth_inds = range(rasters.shape[0])
+
+        # shuffle traces across azimuths
+        if azimuth_shuffle_seed: 
+            collated_rasters = rasters.reshape(rasters.shape[0]*rasters.shape[1],rasters.shape[2],rasters.shape[3])
+            # permute along 1st index 
+            np.random.seed(azimuth_shuffle_seed)
+            collated_rasters = np.random.permutation(collated_rasters)
+            rasters = collated_rasters.reshape(rasters.shape[0],rasters.shape[1],rasters.shape[2],rasters.shape[3])
+        
+        # shuffle trial indices
+        trials_idx = self.aud.trials.values
+        np.random.seed(0)
+        trials_idx = np.random.permutation(trials_idx)
+        split_edges = list(itertools.accumulate(itertools.repeat(int(trials_idx.size/cv_split),cv_split)))
+        split_edges.insert(0,0)
 
         tuning_curves = []
         for cv in range(cv_split):
-            azimuth_times_dict = {}
-            for azimuth in azimuth_options:
-                azimuth_times_dict[('%s_%.0f' % (which,azimuth))] = onset_matrix.sel(
-                    azimuths = azimuth,
-                    trials=trials_idx[split_edges[cv]:split_edges[cv+1]]
-                ).values
-
             responses = {}
-            for k in azimuth_times_dict.keys():
-                t_on = azimuth_times_dict[k]
-                r = get_binned_rasters(self.spikes.times,self.spikes.clusters,clus_ids,t_on,**self.raster_kwargs)
-                stim = r.rasters[:,:,r.tscale>=0]
+            for k in azimuth_inds:
+                curr_idxs = trials_idx[split_edges[cv]:split_edges[cv+1]]
+                stim = rasters[k,curr_idxs,:,:]                 
                 Rmax_stim = np.max(np.abs(stim.mean(axis=0)),axis=1)
-                responses[k] = Rmax_stim
+                responses[azimuths[k]] = Rmax_stim
 
             curr_tuning_curves = pd.DataFrame.from_dict(responses)            
             preferred_tuning_idx = np.argmax(curr_tuning_curves.to_numpy(),axis=1)
 
-            curr_tuning_curves[('preferred_%s_tuning' % which)] = [re.split('_',curr_tuning_curves.columns[i])[-1] for i in preferred_tuning_idx]
+            curr_tuning_curves[('preferred_tuning')] = [re.split('_',curr_tuning_curves.columns[i])[-1] for i in preferred_tuning_idx]
             curr_tuning_curves['cv_number'] = cv
-            curr_tuning_curves = curr_tuning_curves.set_index(clus_ids,drop=True)
-
-
+            curr_tuning_curves = curr_tuning_curves.set_index(self.clus_ids,drop=True)
             tuning_curves.append(curr_tuning_curves)
-
+        
         tuning_curves = pd.concat(tuning_curves)
 
         return tuning_curves
+            
+
+
+
+
+
+            
+
+        
+
+        
+
+        pass 
+
+    #     self
+    #     Rmax_stim = np.max(np.abs(stim.mean(axis=0)),axis=1)
+    #     responses[k] = Rmax_stim
+
+    #     curr_tuning_curves = pd.DataFrame.from_dict(responses)            
+    #     preferred_tuning_idx = np.argmax(curr_tuning_curves.to_numpy(),axis=1)
+
+    #     curr_tuning_curves[('preferred_%s_tuning' % which)] = [re.split('_',curr_tuning_curves.columns[i])[-1] for i in preferred_tuning_idx]
+    #     curr_tuning_curves['cv_number'] = cv
+    #     curr_tuning_curves = curr_tuning_curves.set_index(clus_ids,drop=True)
+    #     print(time.time()-t0)
+
+    #     tuning_curves.append(curr_tuning_curves)
+
+    # tuning_curves = pd.concat(tuning_curves)
+
+    #     return tuning_curves
 
 
 
--- WorkInProgress/Flora/response_test/get_pref_azimuth.py
+++ WorkInProgress/Flora/response_test/get_pref_azimuth.py
@@ -6,8 +6,13 @@
 
 # %% 
 import sys
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
 sys.path.insert(0, r"C:\Users\Flora\Documents\Github\PinkRigs") 
-from Analysis.neural.src.azimuthal_tuning import azimuthal_tuning,get_test_statistic
+from Analysis.neural.src.azimuthal_tuning import azimuthal_tuning,get_discriminability,get_tc_correlations
 session = { 
     'subject':'FT009',
     'expDate': '2021-01-20',
@@ -25,19 +30,58 @@ tuning_curve_params = {
     'contrast': 1,
     'spl': None, 
     'which': tuning_type,
-    'cv_split': 2,
     'subselect_neurons':None,
-    'azimuth_shuffle': None
 }
 
-tc = azi.get_tuning_curves(**tuning_curve_params)
-actual_ = get_test_statistic(tc)
+# %%
+# test for discriminability
 
-# tuning clolumns
-def sample_null_dist(tc_params,my_seed): 
-    tc_params['azimuth_shuffle'] = my_seed
-    tc = azi.get_tuning_curves(**tc_params)
-    null_statistic = get_test_statistic(tc)
+azi.get_rasters_perAzi(**tuning_curve_params)
+tc = azi.get_tuning_curves(cv_split=1,azimuth_shuffle_seed=None)
+d_actual = get_discriminability(tc)
+
+def sample_null_dist(my_seed):
+    tc = azi.get_tuning_curves(cv_split=1,azimuth_shuffle_seed=my_seed)
+    null_statistic = get_discriminability(tc)
     return null_statistic
+
+n_shuffles = 1000
+d_shuff = [sample_null_dist(shuffle_idx)[np.newaxis,:] for shuffle_idx in range(n_shuffles)]
+d_shuff = np.concatenate(d_shuff,axis=0)
+# calculate p value
+d_actual_ =np.tile(d_actual,(n_shuffles,1))
+p_val = (d_shuff>d_actual_).sum(axis=0)/n_shuffles
+
+plt.hist(p_val)
+# also get preferred tuning after 5 cv splits 
+# %%
+tc_pref = azi.get_tuning_curves(cv_split=5,azimuth_shuffle_seed=None)
+c_actual = get_tc_correlations(tc_pref)
+# tuning clolumns
+def sample_null_dist(my_seed):
+    tc = azi.get_tuning_curves(cv_split=5,azimuth_shuffle_seed=my_seed)
+    null_statistic = get_tc_correlations(tc)
+    return null_statistic
+
+n_shuffles = 1000
+c_shuff = [sample_null_dist(shuffle_idx)[np.newaxis,:] for shuffle_idx in range(n_shuffles)]
+c_shuff = np.concatenate(c_shuff,axis=0)
+# calculate p value
+c_actual_ =np.tile(c_actual,(n_shuffles,1))
+p_val = (c_shuff>c_actual_).sum(axis=0)/n_shuffles
+# 
+
+                                                                                                                                                                                                                                                                     # %%
+
+# %%
+# see how it does for some neurons 
+
+
+clusID = 50
+cidx = np.where(azi.clus_ids==clusID)[0][0]
+_,ax = plt.subplots(1,1)
+ax.hist(d_shuff[:,cidx])
+ax.axvline(c_actual[cidx])
+
 
 # %%
