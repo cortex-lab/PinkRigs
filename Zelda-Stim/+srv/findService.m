839eecc91cd21cccd0e0153b1e230997f2153405
Célian Bimbard <54884591+cbimbo@users.noreply.github.com>
2023-09-04 19:22:42 +0100

    Fix findService for poppy

--- Zelda-Stim/+srv/findService.m
+++ Zelda-Stim/+srv/findService.m
@@ -2,34 +2,49 @@ function s = findService(id, varargin)
 %SRV.FINDSERVICE Returns experiment service(s) with specified id(s)
 %   This and EXP.BASICSERVICES has been replaced by SRV.LOADSERVICE. See
 %   also SRV.SERVICE, SRV.LOADSERVICE, SRV.BASICSERVICES.
-%sr
+
 % Part of Rigbox
 
 % 2013-06 CB created
 
 rig = hostname;
 rig = lower(rig);
-rigTime = regexprep(rig,'stim','time');
-rigEphys = regexprep(rig,'stim','ephys');
 
-% Checking which hosts to use, strings should be as they are 
-% known to MC (defined in Rigging/config/remote.mat)
-timelineHost = iff(any(strcmp(id, 'timeline')), {rigTime}, {''});
-micHost = iff(any(strcmp(id, 'microphone')), {rigEphys}, {''});
-eyeCamHost = iff(any(strcmp(id, 'eyeCam')), {rigTime}, {''});
-frontCamHost = iff(any(strcmp(id, 'frontCam')), {rigTime}, {''});
-sideCamHost = iff(any(strcmp(id, 'sideCam')), {rigTime}, {''});
-timelinePort = 1001;
-micPort = 1002;
-eyePort = 1003;
-frontPort = 1004;
-sidePort = 1005;
-
-remoteHosts = [timelineHost, micHost, eyeCamHost, frontCamHost, sideCamHost];
-remotePorts = {timelinePort, micPort, eyePort, frontPort, sidePort};
+if strcmp(rig,'poppy-stim')
+    timelineHost = iff(any(strcmp(id, 'timeline')), {'poppy-timeline'}, {''});
+    % micHost = iff(any(strcmp(id, 'microphone')), {'poppy-ephys'}, {''});
+    micHost = iff(any(strcmp(id, 'microphone')), {'128.40.198.112'}, {''});
+    topCamHost = iff(any(strcmp(id, 'topCam')), {'poppy-timeline'}, {''});
+    
+    timelinePort = 1001;
+    micPort = 1002;
+    topCamPort = 1003;
+    
+    remoteHosts = [timelineHost micHost topCamHost];
+    remotePorts = {timelinePort micPort topCamPort};
+else
+    rigTime = regexprep(rig,'stim','time');
+    rigEphys = regexprep(rig,'stim','ephys');
+    
+    % Checking which hosts to use, strings should be as they are
+    % known to MC (defined in Rigging/config/remote.mat)
+    timelineHost = iff(any(strcmp(id, 'timeline')), {rigTime}, {''});
+    micHost = iff(any(strcmp(id, 'microphone')), {rigEphys}, {''});
+    eyeCamHost = iff(any(strcmp(id, 'eyeCam')), {rigTime}, {''});
+    frontCamHost = iff(any(strcmp(id, 'frontCam')), {rigTime}, {''});
+    sideCamHost = iff(any(strcmp(id, 'sideCam')), {rigTime}, {''});
+    timelinePort = 1001;
+    micPort = 1002;
+    eyePort = 1003;
+    frontPort = 1004;
+    sidePort = 1005;
+    
+    remoteHosts = [timelineHost, micHost, eyeCamHost, frontCamHost, sideCamHost];
+    remotePorts = {timelinePort, micPort, eyePort, frontPort, sidePort};
+end
 
 emp = cellfun(@isempty, remoteHosts);
-% 
+
 MpepHosts = io.MpepUDPDataHosts(remoteHosts(~emp));
 MpepHosts.ResponseTimeout = 60;
 MpepHosts.Id = 'MPEP-Hosts';
