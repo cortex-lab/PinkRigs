bf79203ff4cadd321209178d6d16acd658225833
Célian Bimbard <54884591+cbimbo@users.noreply.github.com>
2023-09-06 11:44:34 +0100

    Make it compatible with empty rec

--- WorkInProgress/Celian/+natim/computeCCA.m
+++ WorkInProgress/Celian/+natim/computeCCA.m
@@ -21,7 +21,7 @@ function [corrWCCA, W, pUall] = computeCCA(spikeData,cc2keep)
     %   Contains the CCA weights
 
     if ~exist('cc2keep','var')
-        cc2keep = 1:75;
+        cc2keep = 1:75; % arbitrary number
     end
     
     % perform CCA
@@ -29,13 +29,15 @@ function [corrWCCA, W, pUall] = computeCCA(spikeData,cc2keep)
     idxall = [];
     clear U S V
     for k = 1:numel(spikeData)
-        s = size(spikeData{k});
-        X = reshape(nanmean(spikeData{k},4),[s(1)*s(2),s(3)]);
-        Xz = zscore(X);
-        [U{k},S{k},V{k}] = svd(X, 'econ');
-        Xw = U{k}*V{k}';
-        Xwall = cat(2,Xwall, Xw);
-        idxall = [idxall, ones(1,s(3))*k];
+        if ~isempty(spikeData{k})
+            s = size(spikeData{k});
+            X = reshape(nanmean(spikeData{k},4),[s(1)*s(2),s(3)]);
+            % Xz = zscore(X);
+            [U{k},S{k},V{k}] = svd(X, 'econ');
+            Xw = U{k}*V{k}';
+            Xwall = cat(2,Xwall, Xw);
+            idxall = [idxall, ones(1,s(3))*k];
+        end
     end
     [Uall,~,~] = svd(Xwall-mean(Xwall), 'econ');
 
@@ -44,18 +46,20 @@ function [corrWCCA, W, pUall] = computeCCA(spikeData,cc2keep)
     % get weight of each neuron on each day, for each component
     pUall = pinv(Uall(:,cc2keep));
     for k = 1:numel(spikeData)
-        s = size(spikeData{k});
-        X = reshape(nanmean(spikeData{k},4),[s(1)*s(2),s(3)]);
-        W{k} = (pUall*X)'; % directly recompute a linear regression of Uall onto the data
+        if ~isempty(spikeData{k})
+            s = size(spikeData{k});
+            X = reshape(nanmean(spikeData{k},4),[s(1)*s(2),s(3)]);
+            W{k} = (pUall*X)'; % directly recompute a linear regression of Uall onto the data
+        else 
+            W{k} = [];
+        end
     end
 
     %% look at correlation of all cells with all the other cells, to see if any is stable...
 
     corrWCCA = cell(numel(spikeData),numel(spikeData));
     for d1 = 1:numel(spikeData)
-        nCd1 = size(spikeData{d1},3);
         for d2 = 1:numel(spikeData)
-            nCd2 = size(spikeData{d2},3);
             if ~isempty(W{d1}) && ~isempty(W{d2})
                 corrWCCA{d1,d2} = corr(W{d1}(:,cc2keep)',W{d2}(:,cc2keep)');
             else
